f2ad581f5273a40f09513279f19d5d167a32c32f
seoAreum <1971236@hansung.ac.kr>
2023-12-30 12:49:36 +0900

    init: 폴더구조 초기세팅

---
+++ src/Router.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./Home/page/HomePage";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
