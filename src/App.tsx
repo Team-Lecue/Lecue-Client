fd5eddae9410be411008e32afce3a8919801c297
Doyun Lee <doyn511@ajou.ac.kr>
2024-01-02 14:50:44 +0900

    init : Theme 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { Global } from '@emotion/react';
+import { Global, ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import Router from './Router';
 import gStyle from './styles/GlobalStyles';
+import theme from './styles/theme';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Global styles={gStyle} />
-      <Router />
+      <ThemeProvider theme={theme}>
+        <Global styles={gStyle} />
+        <Router />
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
