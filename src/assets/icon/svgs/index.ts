3cdaeacf89fbdbd1d16e75f4120c6e9b743a47be
Doyun Lee <doyn511@ajou.ac.kr>
2024-01-04 01:50:20 +0900

    chore: svg 파일명 수정에 따른 index.ts 변경

--- src/assets/icon/svgs/index.ts
+++ src/assets/icon/svgs/index.ts
@@ -1,33 +1,33 @@
 /// <reference types="vite-plugin-svgr/client" />
 
+import BtnFloatingList from './btn_floating_list.svg?react';
+import BtnFloatingPostit from './btn_floating_postit.svg?react';
+import BtnFloatingSticker from './btn_floating_sticker.svg?react';
+import BtnFloatingWrite from './btn_floating_write.svg?react';
 import IcArrowLeft from './ic_arrow_left.svg?react';
-import IcBtnFloatingList from './ic_btn_floating_list.svg?react';
-import IcBtnFloatingPostit from './ic_btn_floating_postit.svg?react';
-import IcBtnFloatingSticker from './ic_btn_floating_sticker.svg?react';
-import IcBtnFloatingWrite from './ic_btn_floating_write.svg?react';
 import IcCrown from './ic_crown.svg?react';
 import IcDate from './ic_date.svg?react';
 import IcHome from './ic_home.svg?react';
-import IcLe from './ic_le.svg?react';
-import IcLogoLecue from './ic_logo_lecue.svg?react';
 import IcNotice from './ic_notice.svg?react';
 import IcSharing from './ic_sharing.svg?react';
-import IcStar from './ic_star.svg?react';
-import IcSticker from './ic_sticker.svg?react';
+import ImgLe from './img_le.svg?react';
+import ImgLogoLecue from './img_logo_lecue.svg?react';
+import ImgStar from './img_star.svg?react';
+import ImgSticker from './img_sticker.svg?react';
 
 export {
+  BtnFloatingList,
+  BtnFloatingPostit,
+  BtnFloatingSticker,
+  BtnFloatingWrite,
   IcArrowLeft,
-  IcBtnFloatingList,
-  IcBtnFloatingPostit,
-  IcBtnFloatingSticker,
-  IcBtnFloatingWrite,
   IcCrown,
   IcDate,
   IcHome,
-  IcLe,
-  IcLogoLecue,
   IcNotice,
   IcSharing,
-  IcStar,
-  IcSticker,
+  ImgLe,
+  ImgLogoLecue,
+  ImgStar,
+  ImgSticker,
 };
