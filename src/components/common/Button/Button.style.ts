322b60e16f477847963d4e943f80327dccf760e4
eunbeann <ds5bean23@gmail.com>
2024-01-03 17:39:00 +0900

    style: CSS 개행 순서 반영

--- src/components/common/Button/Button.style.ts
+++ src/components/common/Button/Button.style.ts
@@ -5,13 +5,13 @@ export type ButtonStyle = 'choose' | 'complete';
 export const CustomButton = styled.button<{ variant: ButtonStyle }>`
   width: 34.4rem;
   height: 6rem;
-  border-radius: 0.625rem;
 
+  border-radius: 0.625rem;
   background-color: ${({ theme, variant }) =>
     variant === 'choose' ? theme.colors.key : theme.colors.BG};
+  color: ${({ theme }) => theme.colors.white};
 
   ${({ theme }) => theme.fonts.Head2_SB_18};
-  color: ${({ theme }) => theme.colors.white};
 
   :disabled {
     background-color: ${({ theme }) => theme.colors.MG};
