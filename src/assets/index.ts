9495a238b073099a0ea1e64f6f7e30cd08ad0cdc
Doyun Lee <doyn511@ajou.ac.kr>
2024-01-04 14:49:07 +0900

    init: assets 폴더구조 변경, svg추가

---
+++ src/assets/index.ts
@@ -0,0 +1,35 @@
+/// <reference types="vite-plugin-svgr/client" />
+
+import BtnFloatingPostit from './button/btn_floating_postit.svg?react';
+import BtnFloatingSticker from './button/btn_floating_sticker.svg?react';
+import BtnFloatingWrite from './button/btn_floating_write.svg?react';
+import BtnFloatingList from './button/button/btn_floating_list.svg?react';
+import IcArrowLeftBlack from './icon/ic_arrow_left_black.svg?react';
+import IcArrowLeftWhite from './icon/ic_arrow_left_white.svg?react';
+import IcCrown from './icon/ic_crown.svg?react';
+import IcDate from './icon/ic_date.svg?react';
+import IcHome from './icon/ic_home.svg?react';
+import IcNotice from './icon/ic_notice.svg?react';
+import IcSharing from './icon/ic_sharing.svg?react';
+import ImgLe from './img/img_le.svg?react';
+import ImgLogoLecue from './img/img_logo_lecue.svg?react';
+import ImgStar from './img/img_star.svg?react';
+import ImgSticker from './img/img_sticker.svg?react';
+
+export {
+  BtnFloatingList,
+  BtnFloatingPostit,
+  BtnFloatingSticker,
+  BtnFloatingWrite,
+  IcArrowLeftBlack,
+  IcArrowLeftWhite,
+  IcCrown,
+  IcDate,
+  IcHome,
+  IcNotice,
+  IcSharing,
+  ImgLe,
+  ImgLogoLecue,
+  ImgStar,
+  ImgSticker,
+};
