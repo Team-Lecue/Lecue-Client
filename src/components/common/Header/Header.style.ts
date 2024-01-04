1af5b0331a346b8f6378ca97da3cd03c604b5b3c
jungwoo3490 <jungwoo3490@naver.com>
2024-01-04 15:06:54 +0900

    feat: Header 컴포넌트 추상화 & 다크모드 추가

--- src/components/common/Header/Header.style.ts
+++ src/components/common/Header/Header.style.ts
@@ -1,21 +1,38 @@
 import styled from '@emotion/styled';
 
-export const HeaderWrapper = styled.header`
+export const HeaderWrapper = styled.header<{ isDarkMode: boolean }>`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
+  position: relative;
 
   height: 5.4rem;
   padding: 0 1.6rem;
 
-  border-bottom: 1px solid ${({ theme }) => theme.colors.BG};
+  border-bottom: 0.1rem solid
+    ${({ isDarkMode, theme }) =>
+      isDarkMode ? theme.colors.white : theme.colors.BG};
+  background-color: ${({ isDarkMode, theme }) =>
+    isDarkMode ? theme.colors.BG : theme.colors.white};
 `;
 
-export const HeaderTitle = styled.p`
+export const HeaderTitle = styled.p<{ isDarkMode: boolean }>`
+  color: ${({ isDarkMode, theme }) =>
+    isDarkMode ? theme.colors.white : theme.colors.BG};
   ${({ theme }) => theme.fonts.E_Title1_SB_18};
 `;
 
-export const HeaderButton = styled.button`
+export const HeaderButtonWrapper = styled.div<{
+  isLeft: boolean;
+}>`
+  position: absolute;
+  ${({ isLeft }) => (isLeft ? 'left' : 'right')}: 1.6rem;
+
   width: 3.2rem;
   height: 3.2rem;
+`;
+
+export const HeaderButton = styled.button`
+  width: 100%;
+  height: 100%;
 `;
--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,29 +1,69 @@
-import { IcHome, IcSharing } from '../../../assets/icon/svgs';
+import { ReactNode } from 'react';
 import * as S from './Header.style';
+import {
+  IcArrowLeftBlack,
+  IcArrowLeftWhite,
+  IcHome,
+  IcSharing,
+} from '../../../assets';
 
 interface HeaderProps {
   headerTitle: string;
+  isDarkMode: boolean;
+  isDetailPage?: boolean;
 }
 
-function Header({ headerTitle }: HeaderProps) {
+interface HeaderButtonProps {
+  isDarkMode: boolean;
+}
+
+function Header({ headerTitle, isDarkMode, isDetailPage }: HeaderProps) {
+  return (
+    <S.HeaderWrapper isDarkMode={isDarkMode}>
+      <S.HeaderButtonWrapper isLeft={true}>
+        {isDetailPage ? <HomeButton /> : <BackButton isDarkMode={isDarkMode} />}
+      </S.HeaderButtonWrapper>
+      <S.HeaderTitle isDarkMode={isDarkMode}>{headerTitle}</S.HeaderTitle>
+      <S.HeaderButtonWrapper isLeft={false}>
+        {isDetailPage ? <ShareButton /> : <></>}
+      </S.HeaderButtonWrapper>
+    </S.HeaderWrapper>
+  );
+}
+
+export function HomeButton() {
   const handleClickHomeButton = () => {
     // 홈으로 이동하는 로직 추후 구현 필요
   };
 
+  return (
+    <S.HeaderButton onClick={handleClickHomeButton}>
+      <IcHome />
+    </S.HeaderButton>
+  );
+}
+
+export function ShareButton() {
   const handleClickShareButton = () => {
     // 공유(?) 페이지로 이동하는 로직 추후 구현 필요
   };
 
   return (
-    <S.HeaderWrapper>
-      <S.HeaderButton onClick={handleClickHomeButton}>
-        <IcHome />
-      </S.HeaderButton>
-      <S.HeaderTitle>{headerTitle}</S.HeaderTitle>
-      <S.HeaderButton onClick={handleClickShareButton}>
-        <IcSharing />
-      </S.HeaderButton>
-    </S.HeaderWrapper>
+    <S.HeaderButton onClick={handleClickShareButton}>
+      <IcSharing />
+    </S.HeaderButton>
+  );
+}
+
+export function BackButton({ isDarkMode }: HeaderButtonProps) {
+  const handleClickBackButton = () => {
+    // 뒤로 이동하는 로직 추후 구현 필요
+  };
+
+  return (
+    <S.HeaderButton onClick={handleClickBackButton}>
+      {isDarkMode ? <IcArrowLeftWhite /> : <IcArrowLeftBlack />}
+    </S.HeaderButton>
   );
 }
 
