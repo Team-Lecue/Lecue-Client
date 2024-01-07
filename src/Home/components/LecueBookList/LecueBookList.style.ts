4fc2569b9d815ec1f1e8a0b14850f62f7bc2ddfd
Doyun Lee <doyn511@ajou.ac.kr>
2024-01-08 00:32:00 +0900

    feat: Home/LequeBookList 컴포넌트 구현

---
+++ src/Home/components/LecueBookList/LecueBookList.style.ts
@@ -0,0 +1,59 @@
+import styled from '@emotion/styled';
+
+export const LecueBookListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+`;
+
+export const Title = styled.header`
+  width: 100%;
+  padding: 1.5rem 0;
+
+  border-color: ${({ theme }) => theme.colors.BG};
+  border-width: 0.1rem 0;
+  border-style: solid;
+  color: ${({ theme }) => theme.colors.BG};
+  ${({ theme }) => theme.fonts.Title1_SB_16};
+
+  text-align: center;
+`;
+
+export const LecueBookList = styled.section`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2.2rem;
+
+  width: 100%;
+  padding: 3rem 1.6rem 2.2rem;
+
+  background-color: ${({ theme }) => theme.colors.key};
+`;
+
+export const LecueBook = styled.li`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  width: 10rem;
+  height: 14rem;
+  gap: 1rem;
+`;
+
+export const BookImage = styled.img`
+  width: 9.8rem;
+  height: 9.8rem;
+
+  border-radius: 50%;
+`;
+
+export const BookTitle = styled.p`
+  width: 80%;
+
+  ${({ theme }) => theme.fonts.E_Body1_SB_14};
+
+  text-align: center;
+  word-wrap: normal;
+  word-break: keep-all;
+`;
---
+++ src/Home/components/LecueBookList/index.tsx
@@ -0,0 +1,97 @@
+import * as S from './LecueBookList.style';
+function LecueBookList() {
+  return (
+    <S.LecueBookListWrapper>
+      <S.Title>인기 레큐북 구경하기</S.Title>
+      <S.LecueBookList>
+        {BOOK_LIST.map((book) => (
+          <S.LecueBook key={book.bookId}>
+            <S.BookImage src={book.favoriteImage} alt="레큐북-이미지" />
+            <S.BookTitle>{book.favoriteName}</S.BookTitle>
+          </S.LecueBook>
+        ))}
+        ;
+      </S.LecueBookList>
+    </S.LecueBookListWrapper>
+  );
+}
+
+export default LecueBookList;
+
+const BOOK_LIST = [
+  {
+    bookId: 0,
+    favoriteName: 'SOPT',
+    favoriteImage: 'https://source.unsplash.com/random/98x98?sig=1',
+  },
+  {
+    bookId: 1,
+    favoriteName: '환승연애 출연진',
+    favoriteImage: 'https://source.unsplash.com/random/98x98?sig=2',
+  },
+  {
+    bookId: 2,
+    favoriteName: 'Vanner',
+    favoriteImage: 'https://source.unsplash.com/random/98x98?sig=3',
+  },
+  {
+    bookId: 3,
+    favoriteName: '침착맨 생카 데이쥬',
+    favoriteImage: 'https://source.unsplash.com/random/98x98?sig=4',
+  },
+  {
+    bookId: 4,
+    favoriteName: '빠니보틀',
+    favoriteImage: 'https://source.unsplash.com/random/98x98?sig=5',
+  },
+  {
+    bookId: 5,
+    favoriteName: '레오제이',
+    favoriteImage: 'https://source.unsplash.com/random/98x98?sig=6',
+  },
+  {
+    bookId: 6,
+    favoriteName: '곽튜브',
+    favoriteImage: 'https://source.unsplash.com/random/98x98?sig=7',
+  },
+  {
+    bookId: 7,
+    favoriteName: '재롱잔치',
+    favoriteImage: 'https://source.unsplash.com/random/98x98?sig=8',
+  },
+  {
+    bookId: 8,
+    favoriteName: '주호다',
+    favoriteImage: 'https://source.unsplash.com/random/98x98?sig=9',
+  },
+  {
+    bookId: 9,
+    favoriteName: '1분요리 뚝딱이형',
+    favoriteImage: 'https://source.unsplash.com/random/98x98?sig=10',
+  },
+  {
+    bookId: 10,
+    favoriteName: '송이송이',
+    favoriteImage: 'https://source.unsplash.com/random/98x98?sig=11',
+  },
+  {
+    bookId: 11,
+    favoriteName: '상윤쓰',
+    favoriteImage: 'https://source.unsplash.com/random/98x98?sig=12',
+  },
+  {
+    bookId: 12,
+    favoriteName: '1분요리 뚝딱이형',
+    favoriteImage: 'https://source.unsplash.com/random/98x98?sig=13',
+  },
+  {
+    bookId: 13,
+    favoriteName: '송이송이',
+    favoriteImage: 'https://source.unsplash.com/random/98x98?sig=14',
+  },
+  {
+    bookId: 14,
+    favoriteName: '상윤쓰',
+    favoriteImage: 'https://source.unsplash.com/random/98x98?sig=15',
+  },
+];
