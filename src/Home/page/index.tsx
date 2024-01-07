f6b67b74229b938b3594f5f1cf62dcd742a27908
Doyun Lee <doyn511@ajou.ac.kr>
2024-01-08 00:36:21 +0900

    feat: 홈 뷰 컴포넌트 import

---
+++ src/Home/page/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import LecueBookList from '../components/LecueBookList';
+import Main from '../components/Main';
+
+function Home() {
+  return (
+    <React.Fragment>
+      <Main />
+      <LecueBookList />
+    </React.Fragment>
+  );
+}
+
+export default Home;
