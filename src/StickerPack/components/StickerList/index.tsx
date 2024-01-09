f7a75094560f9c6c8a03287cc7af67723ac13d1b
eunbenn <ds5bean23@gmail.com>
2024-01-10 04:13:14 +0900

    chore: 목 데이터 실제 데이터로 변경

--- src/StickerPack/components/StickerList/index.tsx
+++ src/StickerPack/components/StickerList/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import useGetStickerPack from '../../hooks/useGetStickerPack';
+import { stickerPackType } from '../../type/stickerPackType';
 import * as S from './StickerList.style';
 
 interface StickerListProps {
@@ -9,10 +11,11 @@ interface StickerListProps {
 
 function StickerList(props: StickerListProps) {
   const { isSelectedId, handleStickerClick } = props;
+  const { stickerPack } = useGetStickerPack();
 
   return (
     <S.Wrapper>
-      {MockSticker.map((data) => (
+      {stickerPack.map((data: stickerPackType) => (
         <React.Fragment key={data.stickerCategory}>
           <S.Title>{data.stickerCategory}</S.Title>
           <S.StickerGridWrapper>
@@ -25,7 +28,7 @@ function StickerList(props: StickerListProps) {
                 <S.ImageComponent
                   src={sticker.stickerImage}
                   alt="스티커 이미지"
-              />
+                />
               </S.ImageWrapper>
             ))}
           </S.StickerGridWrapper>
@@ -36,140 +39,3 @@ function StickerList(props: StickerListProps) {
 }
 
 export default StickerList;
-
-const MockSticker = [
-  {
-    stickerCategory: '레큐잉',
-    stickerInfoList: [
-      {
-        stickerId: 1,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=1',
-      },
-      {
-        stickerId: 2,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=2',
-      },
-      {
-        stickerId: 3,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=3',
-      },
-      {
-        stickerId: 4,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=4',
-      },
-      {
-        stickerId: 5,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=5',
-      },
-      {
-        stickerId: 6,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=6',
-      },
-      {
-        stickerId: 7,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=7',
-      },
-      {
-        stickerId: 8,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=8',
-      },
-      {
-        stickerId: 9,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=9',
-      },
-      {
-        stickerId: 10,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=10',
-      },
-      {
-        stickerId: 11,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=11',
-      },
-      {
-        stickerId: 12,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=12',
-      },
-      {
-        stickerId: 13,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=13',
-      },
-      {
-        stickerId: 14,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=14',
-      },
-      {
-        stickerId: 15,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=15',
-      },
-      {
-        stickerId: 16,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=16',
-      },
-      {
-        stickerId: 17,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=17',
-      },
-      {
-        stickerId: 18,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=18',
-      },
-      {
-        stickerId: 19,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=19',
-      },
-      {
-        stickerId: 20,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=20',
-      },
-    ],
-  },
-  {
-    stickerCategory: '큐비링',
-    stickerInfoList: [
-      {
-        stickerId: 91,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=1',
-      },
-      {
-        stickerId: 92,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=2',
-      },
-      {
-        stickerId: 93,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=3',
-      },
-      {
-        stickerId: 94,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=4',
-      },
-      {
-        stickerId: 95,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=5',
-      },
-      {
-        stickerId: 96,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=6',
-      },
-      {
-        stickerId: 97,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=7',
-      },
-      {
-        stickerId: 98,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=8',
-      },
-      {
-        stickerId: 99,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=9',
-      },
-      {
-        stickerId: 910,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=10',
-      },
-      {
-        stickerId: 911,
-        stickerImage: 'https://source.unsplash.com/random/100x100?sig=11',
-      },
-    ],
-  },
-];
