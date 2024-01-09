a192806d07d44865bcd5b6cc00e592e57148103e
Doyun Lee <doyn511@ajou.ac.kr>
2024-01-09 21:11:23 +0900

    feat: LecueExplain 컴포넌트 구현

--- src/Login/components/LecueExplain/index.tsx
+++ src/Login/components/LecueExplain/index.tsx
@@ -1,13 +1,34 @@
-import { ImgKakaoLogo } from '../../../assets';
+import {
+  ImgKakao01,
+  ImgKakao02,
+  ImgKakao03,
+  ImgKakaoLine,
+  ImgKakaoLogo,
+} from '../../../assets';
 import * as S from './LecueExplain.style';
 function LecueExplain() {
   return (
     <S.GirdWrapper>
-      <S.GridElement></S.GridElement>
+      <S.GridElement />
       <S.GridElement>
         <ImgKakaoLogo />
       </S.GridElement>
-      <S.GridElement></S.GridElement>
+      <S.GridElement>
+        <S.CommentWrapper>
+          <S.Comment>
+            <ImgKakao01 />
+          </S.Comment>
+          <S.Comment>
+            <ImgKakao02 />
+          </S.Comment>
+          <S.Comment>
+            <ImgKakao03 />
+          </S.Comment>
+        </S.CommentWrapper>
+        <S.Line>
+          <ImgKakaoLine />
+        </S.Line>
+      </S.GridElement>
     </S.GirdWrapper>
   );
 }
