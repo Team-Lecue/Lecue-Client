2cef340b50ca7333d01f55827aed551110f9b5fc
Doyun Lee <doyn511@ajou.ac.kr>
2024-01-10 04:38:44 +0900

    feat: 로그인 버튼 컴포넌트 구현

--- src/Login/page/index.tsx
+++ src/Login/page/index.tsx
@@ -1,12 +1,16 @@
+import Comment from '../components/Comment';
 import Header from '../components/Header';
-import LecueExplain from '../components/LecueExplain';
+import LecueLogo from '../components/LecueLogo';
+import LoginBtnContainer from '../components/LoginBtnContainer';
 import * as S from './Login.style';
 
 function Login() {
   return (
     <S.LoginWrapper>
       <Header />
-      <LecueExplain />
+      <LecueLogo />
+      <Comment />
+      <LoginBtnContainer />
     </S.LoginWrapper>
   );
 }
