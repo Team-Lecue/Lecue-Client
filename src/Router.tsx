2b7df3f6e05e08796a24b832a1ffaadfb41e7e23
Doyun Lee <doyn511@ajou.ac.kr>
2024-01-10 06:37:00 +0900

    feat: 라우터 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import HomePage from './Home/page/HomePage';
+import Register from './Register/page';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
