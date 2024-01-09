1d587130c3fce766ea166bba6f610be9732d86d6
Doyun Lee <doyn511@ajou.ac.kr>
2024-01-10 04:30:32 +0900

    feat: width 단위 변경

--- src/Login/components/LecueLogo/LecueLogo.style.ts
+++ src/Login/components/LecueLogo/LecueLogo.style.ts
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 
 export const LogoWrapper = styled.section`
   display: flex;
+  justify-content: center;
 
   width: 100vw;
   height: 11.2rem;
@@ -10,7 +11,7 @@ export const LogoWrapper = styled.section`
 `;
 
 export const Line = styled.div`
-  width: 30%;
+  width: 11.2rem;
 
   border-right: 0.1rem solid ${({ theme }) => theme.colors.black};
 `;
@@ -19,6 +20,6 @@ export const Logo = styled.i`
   display: flex;
   justify-content: end;
 
-  width: 70%;
+  width: 26.3rem;
   padding: 1.8rem 1.4rem 2.2rem 2.9rem;
 `;
