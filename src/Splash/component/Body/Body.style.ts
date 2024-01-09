f451d0473b368de51d5c6a0b0c7f5b71c1670849
seoAreum <1971236@hansung.ac.kr>
2024-01-09 21:42:33 +0900

    feat: 스플래시 뷰 바디 퍼블리싱

---
+++ src/Splash/component/Body/Body.style.ts
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const LottieWrapper = styled.section`
+  width: 100vw;
+  height: 39rem;
+`;
---
+++ src/Splash/component/Body/index.tsx
@@ -0,0 +1,13 @@
+// import Lottie from 'lottie-react'
+
+import * as S from './Body.style';
+
+function Body() {
+  return (
+    <S.LottieWrapper>
+        {/* <Lottie animationData={}/> */}
+    </S.LottieWrapper>
+  )
+}
+
+export default Body;
