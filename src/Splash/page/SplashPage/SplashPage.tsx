9706b8c2458ae6894e7b8e07c1c6307cac3f5920
seoAreum <1971236@hansung.ac.kr>
2024-01-09 21:43:06 +0900

    feat: 스플래시 페이지 구현

---
+++ src/Splash/page/SplashPage/SplashPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Body from '../../component/Body';
+import Bottom from '../../component/Bottom';
+import Header from '../../component/Header';
+
+function SplashPage() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Body />
+      <Bottom />
+    </React.Fragment>
+  );
+}
+
+export default SplashPage;
