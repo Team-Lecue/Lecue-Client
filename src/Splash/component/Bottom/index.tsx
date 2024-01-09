d120b6211d6b839ec4457b57072910d16a80bd43
seoAreum <1971236@hansung.ac.kr>
2024-01-09 21:42:14 +0900

    feat: 스플래시뷰 헤더 퍼블리싱

---
+++ src/Splash/component/Bottom/index.tsx
@@ -0,0 +1,16 @@
+import * as S from './Bottom.style';
+
+function Bottom() {
+  return (
+    <S.BottomWrapper>
+      <S.TextWrapper>
+        <S.Text>지금까지 120개의</S.Text>
+        <S.Text>포스트잇이 붙여졌어요!</S.Text>
+      </S.TextWrapper>
+
+      <S.StartBtn type="button">시작하기</S.StartBtn>
+    </S.BottomWrapper>
+  );
+}
+
+export default Bottom;
---
+++ src/Splash/component/Header/Header.style.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: left;
+
+  width: 100vw;
+`;
+
+export const Wrapper = styled.div`
+  margin: 1.8rem 0 0.8rem 1.8rem;
+`;
