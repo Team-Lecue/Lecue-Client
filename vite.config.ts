4c9290a3a237770cf5718a936186995125cf3764
eunbeann <ds5bean23@gmail.com>
2024-01-10 00:34:59 +0900

    feat: docker deploy test

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
   plugins: [react(), svgr()],
 });
