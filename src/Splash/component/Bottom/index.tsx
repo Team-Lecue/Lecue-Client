b52e781b808ad123c4af1656217509d8ed25d359
seoAreum <1971236@hansung.ac.kr>
2024-01-10 00:43:06 +0900

    feat: 노트 개수 가상의 상수로 대체

--- src/Splash/component/Bottom/index.tsx
+++ src/Splash/component/Bottom/index.tsx
@@ -1,10 +1,12 @@
 import * as S from './Bottom.style';
 
 function Bottom() {
+  const noteNum = 30;
+
   return (
     <S.BottomWrapper>
       <S.TextWrapper>
-        <S.Text>지금까지 120개의</S.Text>
+        <S.Text>지금까지 {noteNum}개의</S.Text>
         <S.Text>포스트잇이 붙여졌어요!</S.Text>
       </S.TextWrapper>
 
