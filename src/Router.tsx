d5de8f4da0c4a2f417b091b95ddd7e5eeb16c01f
seoAreum <1971236@hansung.ac.kr>
2024-01-10 15:47:26 +0900

    feat: 라우팅 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import HomePage from './Home/page/HomePage';
+import LecueNotePage from './LecueNote/page/LeceuNotePage';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="create-note" element={<LecueNotePage />} />
       </Routes>
     </BrowserRouter>
   );
