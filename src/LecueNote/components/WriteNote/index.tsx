16158fd35da0dada4cf6a3c57a6b76359d5de4e8
seoAreum <1971236@hansung.ac.kr>
2024-01-11 00:31:49 +0900

    chore: UX 라이팅 반영

--- src/LecueNote/components/WriteNote/index.tsx
+++ src/LecueNote/components/WriteNote/index.tsx
@@ -4,7 +4,7 @@ function WriteNote() {
   return (
     <S.Wrapper>
       <S.LecueNote></S.LecueNote>
-      <S.Notice>욕설/비속어는 자동 필터링됩니다.</S.Notice>
+      <S.Notice>*욕설/비속어는 자동 필터링됩니다.</S.Notice>
     </S.Wrapper>
   );
 }
