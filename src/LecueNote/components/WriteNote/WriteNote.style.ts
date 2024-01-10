2958528e6ba4fdcf5410dd89a697b14ae0f11954
seoAreum <1971236@hansung.ac.kr>
2024-01-10 16:18:12 +0900

    feat: 포스트잇 작성 컴포넌트 퍼블리싱

--- src/LecueNote/components/WriteNote/WriteNote.style.ts
+++ src/LecueNote/components/WriteNote/WriteNote.style.ts
@@ -2,8 +2,20 @@ import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
+
+  gap: 0.4rem;
 `;
 
-export const Contents = styled.p`
-  color: blue;
+export const LecueNote = styled.article`
+  width: 34.2rem;
+  height: 34.2rem;
+  border-radius: 0.6rem;
+
+  background-color: ${({ theme }) => theme.colors.sub_pink};
+`;
+
+export const Notice = styled.p`
+  color: ${({ theme }) => theme.colors.key};
+  ${({ theme }) => theme.fonts.Caption1_R_12};
 `;
--- src/LecueNote/components/WriteNote/index.tsx
+++ src/LecueNote/components/WriteNote/index.tsx
@@ -3,7 +3,8 @@ import * as S from './WriteNote.style';
 function WriteNote() {
   return (
     <S.Wrapper>
-      <S.Contents>WriteNote</S.Contents>
+      <S.LecueNote></S.LecueNote>
+      <S.Notice>욕설/비속어는 자동 필터링됩니다.</S.Notice>
     </S.Wrapper>
   );
 }
