0b2bc29b53efd26dc44f60ebe8cc0c3251ea14a2
seoAreum <1971236@hansung.ac.kr>
2024-01-10 16:18:50 +0900

    style: 포스트잇 생성 관련 레이아웃 조정

--- src/LecueNote/components/CreateNote/CreateNote.style.ts
+++ src/LecueNote/components/CreateNote/CreateNote.style.ts
@@ -1,6 +1,13 @@
 import styled from '@emotion/styled';
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+
+  height: 100%;
+
+  margin: 2.4rem 1.7rem 4rem;
+
+  gap: 3.2rem;
 `;
