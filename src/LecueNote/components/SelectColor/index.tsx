cbf2e150cdd234537c58ec0c2e03bfac0a04dac3
seoAreum <1971236@hansung.ac.kr>
2024-01-11 01:55:25 +0900

    chore: 기존 컴포넌트를 추상화한 컴포넌트로 변경

--- src/LecueNote/components/SelectColor/index.tsx
+++ src/LecueNote/components/SelectColor/index.tsx
@@ -1,37 +1,57 @@
 import { useState } from 'react';
+import { BG_COLOR_CHART, TEXT_COLOR_CHART } from '../../constants/ColorChart';
+import ShowColorChart from '../ShowColorChart';
 import * as S from './SelectColor.style';
-import TextColor from '../TextColor';
-import BgColor from '../BgColor';
 
 function SelectColor() {
-  const [isClicked, setIsClicked] = useState('텍스트색');
+  const CATEGORY = ['텍스트색', '배경색'];
+  const [clickedCategory, setclickedCategory] = useState('텍스트색');
+  const [clickedTextColor, setClickedTextColor] = useState(TEXT_COLOR_CHART[0]);
+  const [clickedBgColor, setclickedBgColor] = useState(BG_COLOR_CHART[0]);
 
   const handleClickCategory = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => {
-    setIsClicked(e.currentTarget.innerHTML);
+    setclickedCategory(e.currentTarget.innerHTML);
+  };
+
+  const handleClickedColorBtn = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    clickedCategory === '텍스트색'
+      ? setClickedTextColor(e.currentTarget.id)
+      : setclickedBgColor(e.currentTarget.id);
   };
 
   return (
     <S.Wrapper>
       <S.CategoryWrapper>
-        <S.Category
-          type="button"
-          variant={isClicked === '텍스트색'}
-          onClick={handleClickCategory}
-        >
-          텍스트색
-        </S.Category>
-        <S.Category
-          type="button"
-          variant={isClicked === '배경색'}
-          onClick={handleClickCategory}
-        >
-          배경색
-        </S.Category>
+        {CATEGORY.map((it) => {
+          return (
+            <S.Category
+              type="button"
+              variant={clickedCategory === it}
+              onClick={handleClickCategory}
+            >
+              {it}
+            </S.Category>
+          );
+        })}
       </S.CategoryWrapper>
 
-      {isClicked === '텍스트색' ? <TextColor /> : <BgColor />}
+      {clickedCategory === '텍스트색' ? (
+        <ShowColorChart
+          colorChart={TEXT_COLOR_CHART}
+          state={clickedTextColor}
+          handleFn={handleClickedColorBtn}
+        />
+      ) : (
+        <ShowColorChart
+          colorChart={BG_COLOR_CHART}
+          state={clickedBgColor}
+          handleFn={handleClickedColorBtn}
+        />
+      )}
     </S.Wrapper>
   );
 }
