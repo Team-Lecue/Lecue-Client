2ecccf6493e0b96b7d6ca96dd8e52e5bb16363c2
seoAreum <1971236@hansung.ac.kr>
2024-01-10 21:16:10 +0900

    feat: 색깔차트 상수 배열 정의

---
+++ src/LecueNote/constants/ColorChart.tsx
@@ -0,0 +1,16 @@
+export const TEXT_COLOR_CHART = ['#191919', '#FFF'];
+
+export const BG_COLOR_CHART = [
+  '#EFB6B6',
+  '#E5E2CE',
+  '#F8E99A',
+  '#85CEAF',
+  '#B3CBE8',
+  '#929DD9',
+  '#FE394C',
+  '#9852F9',
+  '#FFD600',
+  '#98ED4D',
+  '#FF71B3',
+  '#CCC',
+];
