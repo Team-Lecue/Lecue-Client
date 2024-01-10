9ab3c9b0246f92953323454be71d8a16609a024a
seoAreum <1971236@hansung.ac.kr>
2024-01-10 21:18:09 +0900

    feat: 글자색 컬러차트 퍼블리싱

--- src/LecueNote/components/TextColor/index.tsx
+++ src/LecueNote/components/TextColor/index.tsx
@@ -1,9 +1,29 @@
+import { useState } from 'react';
+import { TEXT_COLOR_CHART } from '../../constants/ColorChart';
 import * as S from './TextColor.style';
 
 function TextColor() {
+  const [clickedTextColor, setClickedTextColor] = useState(TEXT_COLOR_CHART[0]);
+
+  const handleClickedColorBtn = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    setClickedTextColor(e.currentTarget.id);
+  };
+
   return (
     <S.Wrapper>
-      <S.Contents>TextColor</S.Contents>
+      {TEXT_COLOR_CHART.map((colorCode) => (
+        <S.ColorWrapper>
+          <S.Color
+            type="button"
+            id={colorCode}
+            variant={clickedTextColor === colorCode}
+            $colorCode={colorCode}
+            onClick={handleClickedColorBtn}
+          ></S.Color>
+        </S.ColorWrapper>
+      ))}
     </S.Wrapper>
   );
 }
