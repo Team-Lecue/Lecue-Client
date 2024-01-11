cbc6865b7bbfa674dbdd8b28ad500ce769a31a26
eunbenn <ds5bean23@gmail.com>
2024-01-11 17:56:18 +0900

    feat: healthCheck 테스트 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import DetailPage from './Detail/page/DetailPage';
+import HealthTest from './HealthTest';
 import HomePage from './Home/page/HomePage';
 import Login from './Login/page';
 import StickerPack from './StickerPack/page/StickerPack';
@@ -13,6 +14,7 @@ function Router() {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/sticker-pack" element={<StickerPack />} />
         <Route path="/detail" element={<DetailPage />} />
+        <Route path="/test" element={<HealthTest />} />
       </Routes>
     </BrowserRouter>
   );
