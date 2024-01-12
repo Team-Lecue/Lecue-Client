cae590bd2dae8645e1bdceed0d2dce8bbebc9b24
seoAreum <80264647+Arooming@users.noreply.github.com>
2024-01-12 23:04:24 +0900

    Update src/LecueNote/components/ShowColorChart/ShowColorChart.style.ts
    Co-authored-by: Doyun Lee <108219121+doyn511@users.noreply.github.com>

--- src/LecueNote/components/ShowColorChart/ShowColorChart.style.ts
+++ src/LecueNote/components/ShowColorChart/ShowColorChart.style.ts
@@ -6,7 +6,7 @@ export const Wrapper = styled.div`
   justify-content: flex-start;
   align-items: center;
 
-  padding: 0.5rem 0 0.7rem 0.3rem;
+  padding: 0.5rem 0.1rem 0.7rem 0.3rem;
 
   overflow-x: scroll;
 
