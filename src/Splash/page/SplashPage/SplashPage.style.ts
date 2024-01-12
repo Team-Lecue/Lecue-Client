51b83b3d836291fbd16303b459cfff76678f0ca5
seoAreum <1971236@hansung.ac.kr>
2024-01-12 20:07:14 +0900

    fix: 레이아웃 세로 중앙정렬 해제

--- src/Splash/page/SplashPage/SplashPage.style.ts
+++ src/Splash/page/SplashPage/SplashPage.style.ts
@@ -2,10 +2,11 @@ import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
   flex-direction: column;
 
   width: 100vw;
   height: 100dvh;
+
+  margin: 3rem 0 2rem;
 `;
