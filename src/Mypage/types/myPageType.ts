caea7c6321ecb7ab601f1676778742a0f5baaaf4
Doyun Lee <doyn511@ajou.ac.kr>
2024-01-13 23:59:27 +0900

    feat: Mypage type 생성

---
+++ src/Mypage/types/myPageType.ts
@@ -0,0 +1,37 @@
+export interface LecueBookProps {
+  key: number;
+  favoriteName: string;
+  title: string;
+  bookDate: string;
+  noteNum: number;
+}
+
+export interface LecueNoteProps {
+  key: number;
+  favoriteName: string;
+  title: string;
+  noteDate: string;
+  content: string;
+  noteTextColor?: number;
+  noteBackgroundColor?: number;
+  noteBackgroundImage?: string;
+}
+
+export interface LecueBookType {
+  bookId: number;
+  favoriteName: string;
+  title: string;
+  bookDate: string;
+  noteNum: number;
+}
+
+export interface LecueNoteType {
+  noteId: number;
+  favoriteName: string;
+  title: string;
+  noteDate: string;
+  content: string;
+  noteTextColor?: number;
+  noteBackgroundColor?: number;
+  noteBackgroundImage?: string;
+}
