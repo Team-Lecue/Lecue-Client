7a37a8235fbf0aa4ff17b5959abf992e41c07803
Doyun Lee <doyn511@ajou.ac.kr>
2024-01-14 00:19:16 +0900

    feat: 레큐노트 개수 자릿수 계산 함수 추가

--- src/Mypage/components/LecueBook/index.tsx
+++ src/Mypage/components/LecueBook/index.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from 'react';
+
 import { IcWaste } from '../../../assets';
 import { LecueBookProps } from '../../types/myPageType';
 import * as S from './LecueBook.style';
 
 function LecueBook(props: LecueBookProps) {
+  const [noteCount, setNoteCount] = useState('');
   const { favoriteName, title, bookDate, noteNum } = props;
+
+  const convertNoteCount = (noteNum: number) => {
+    setNoteCount(noteNum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+  };
+
+  useEffect(() => {
+    convertNoteCount(noteNum);
+  });
 
   return (
     <S.Wrapper>
@@ -16,7 +27,7 @@ function LecueBook(props: LecueBookProps) {
       <S.Title>{title}</S.Title>
       <S.Footer>
         <S.Date>{bookDate}</S.Date>
-        <S.Count>{noteNum}개</S.Count>
+        <S.Count>{noteCount}개</S.Count>
       </S.Footer>
     </S.Wrapper>
   );
