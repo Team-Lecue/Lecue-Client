386c7ab5ddd371652e91f326b9fc734d240ce228
eunbenn <ds5bean23@gmail.com>
2024-01-14 02:05:16 +0900

    chore: Router 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,7 @@ import DetailPage from './Detail/page/DetailPage';
 import HealthTest from './HealthTest';
 import HomePage from './Home/page/HomePage';
 import Login from './Login/page';
+import StickerAttach from './StickerAttach/page';
 import StickerPack from './StickerPack/page/StickerPack';
 
 function Router() {
@@ -15,6 +16,7 @@ function Router() {
         <Route path="/sticker-pack" element={<StickerPack />} />
         <Route path="/detail" element={<DetailPage />} />
         <Route path="/test" element={<HealthTest />} />
+        <Route path="/sticker-attach" element={<StickerAttach />} />
       </Routes>
     </BrowserRouter>
   );
