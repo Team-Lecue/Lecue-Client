9acf7fb92b85529a18ec9a8a5fda3c3f20a6dbad
seoAreum <1971236@hansung.ac.kr>
2024-01-13 17:42:36 +0900

    feat: 레큐노트 전반적인 레이아웃 및 선택 텍스트 컬러 반영{

--- src/LecueNote/components/WriteNote/index.tsx
+++ src/LecueNote/components/WriteNote/index.tsx
@@ -1,10 +1,19 @@
 import { WriteNoteProps } from '../../type/lecueNoteType';
 import * as S from './WriteNote.style';
 
-function WriteNote({ clickedBgColor }: WriteNoteProps) {
+function WriteNote({ clickedBgColor, clickedTextColor }: WriteNoteProps) {
+  const nickname = '와라라라랄라';
+
   return (
     <S.Wrapper>
-      <S.LecueNote $bgColor={clickedBgColor}></S.LecueNote>
+      <S.LecueNote $bgColor={clickedBgColor}>
+        <S.Nickname>{nickname}</S.Nickname>
+        <S.Contents $textColor={clickedTextColor} />
+        <S.BottomContentsWrapper>
+          <S.Date>2020.10.23</S.Date>
+          <S.Counter>(1/1000)</S.Counter>
+        </S.BottomContentsWrapper>
+      </S.LecueNote>
       <S.Notice>*욕설/비속어는 자동 필터링됩니다.</S.Notice>
     </S.Wrapper>
   );
