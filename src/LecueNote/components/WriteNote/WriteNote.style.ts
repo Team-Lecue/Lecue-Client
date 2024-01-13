97a2d133e9f78e8358cec047ea1ff78aa4b2b8ab
seoAreum <1971236@hansung.ac.kr>
2024-01-13 17:41:54 +0900

    style: 레큐노트 내부 전반적인 스타일링 적용

--- src/LecueNote/components/WriteNote/WriteNote.style.ts
+++ src/LecueNote/components/WriteNote/WriteNote.style.ts
@@ -7,11 +7,50 @@ export const Wrapper = styled.div`
 `;
 
 export const LecueNote = styled.article<{ $bgColor: string }>`
+  display: flex;
+  flex-direction: column;
+
   width: 100%;
   height: calc(100dvh - 33.2rem);
 
   border-radius: 0.6rem;
   background-color: ${({ $bgColor }) => $bgColor};
+`;
+
+export const Nickname = styled.p`
+  margin: 2rem 0 1rem 2rem;
+
+  ${({ theme }) => theme.fonts.Head1_B_20}
+`;
+
+export const Contents = styled.textarea<{ $textColor: string }>`
+  width: calc(100% - 4rem);
+  height: 100%;
+  margin: 0 2rem 2rem;
+
+  border: none;
+  ${({ theme }) => theme.fonts.Body1_R_16};
+  background-color: transparent;
+  color: ${({ $textColor }) => $textColor};
+`;
+
+export const BottomContentsWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  width: calc(100% - 4rem);
+  height: 1.7rem;
+  margin: 0 2rem 2rem;
+`;
+
+export const Date = styled.p`
+  ${({ theme }) => theme.fonts.E_Body2_R_14};
+  color: ${({ theme }) => theme.colors.DG50};
+`;
+
+export const Counter = styled.p`
+  ${({ theme }) => theme.fonts.E_Body2_R_14};
+  color: ${({ theme }) => theme.colors.DG};
 `;
 
 export const Notice = styled.p`
