9ca3a2296236b72460d6982ef6e087dcf4ece401
seoAreum <1971236@hansung.ac.kr>
2024-01-14 05:03:38 +0900

    feat: 공통모달 큰 틀 구현

--- src/components/common/Modal/CommonModalForm/index.tsx
+++ src/components/common/Modal/CommonModalForm/index.tsx
@@ -1,9 +1,45 @@
+import { ImgModalEx } from '../../../../assets';
 import * as S from './CommonModalForm.style';
 
-function CommonModalForm() {
+interface CommonModalFormProps {
+  onClose: () => void;
+  title: string;
+  subTitle1: string;
+  subTitle2: string;
+  leftBtn: string;
+  rightBtn: string;
+}
+
+function CommonModalForm({
+  onClose,
+  title,
+  subTitle1,
+  subTitle2,
+  leftBtn,
+  rightBtn,
+}: CommonModalFormProps) {
   return (
     <S.Wrapper>
-      <S.Contents>CommonModalForm</S.Contents>
+      <S.Contents>
+        <S.ImgWrapper>
+          <ImgModalEx />
+        </S.ImgWrapper>
+
+        <S.Title>{title}</S.Title>
+        <S.SubTitleWrapper>
+          <S.SubTitle>{subTitle1}</S.SubTitle>
+          <S.SubTitle>{subTitle2}</S.SubTitle>
+        </S.SubTitleWrapper>
+
+        <S.BtnWrapper>
+          <S.Button type="button" variant="stop" onClick={onClose}>
+            {leftBtn}
+          </S.Button>
+          <S.Button type="button" variant="continue">
+            {rightBtn}
+          </S.Button>
+        </S.BtnWrapper>
+      </S.Contents>
     </S.Wrapper>
   );
 }
