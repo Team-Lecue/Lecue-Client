f645e54bcd2556d7f5046063f893cf8834191db0
Doyun Lee <doyn511@ajou.ac.kr>
2024-01-14 00:00:48 +0900

    feat: DATA 반영

--- src/Mypage/components/LecueBook/index.tsx
+++ src/Mypage/components/LecueBook/index.tsx
@@ -1,35 +1,25 @@
 import { IcWaste } from '../../../assets';
+import { LecueBookProps } from '../../types/myPageType';
 import * as S from './LecueBook.style';
-function LecueBook() {
+
+function LecueBook(props: LecueBookProps) {
+  const { favoriteName, title, bookDate, noteNum } = props;
+
   return (
     <S.Wrapper>
       <S.Header>
-        {/* <p>{favoriteName}</p> */}
-        <S.Name>Lecue</S.Name>
+        <S.Name>{favoriteName}</S.Name>
         <S.TrashBtn>
           <IcWaste />
         </S.TrashBtn>
       </S.Header>
-      {/* <p>{title}</p> */}
-      <S.Title>레큐의 탄생을 축하해!</S.Title>
+      <S.Title>{title}</S.Title>
       <S.Footer>
-        {/* <p>{bookDate}</p> */}
-        <S.Date>2024.01.12</S.Date>
-        {/* <p>{noteNum}</p> */}
-        <S.Count>28760개</S.Count>
+        <S.Date>{bookDate}</S.Date>
+        <S.Count>{noteNum}개</S.Count>
       </S.Footer>
     </S.Wrapper>
   );
 }
 
 export default LecueBook;
-
-// {
-//   bookId: 1,
-//   bookUuid: 'ee4f66f9-9cf4-4b28-90f4-f71d0ecba021',
-//   favoriteName: 'Lecue',
-//   title: '레큐의 탄생을 축하해!',
-//   bookDate: '2024.01.12',
-//   bookBackgroundColor: 1,
-//   noteNum: 28760,
-// },
--- src/Mypage/components/LecueList/index.tsx
+++ src/Mypage/components/LecueList/index.tsx
@@ -1,34 +1,78 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
+import { BOOK_LIST, NOTE_LIST } from '../../constants/DATA';
+import { LecueBookType, LecueNoteType } from '../../types/myPageType';
+import LecueBook from '../LecueBook';
+import LecueNote from '../LecueNote';
 import * as S from './LecueList.style';
 
 function LecueList() {
   const [clickedBtn, setClickedBtn] = useState(true);
+  const [noteCount, setNoteCount] = useState(0);
+  const [bookCount, setBookCount] = useState(0);
 
-  const handleClickBtn = (prev: boolean) => {
-    setClickedBtn(!prev);
+  const handleClickNoteBtn = () => {
+    setClickedBtn(true);
   };
+
+  const handleClickBookBtn = () => {
+    setClickedBtn(false);
+  };
+
+  const numberCount = (NOTE: LecueNoteType[], BOOK: LecueBookType[]) => {
+    setNoteCount(NOTE.length);
+    setBookCount(BOOK.length);
+  };
+
+  useEffect(() => {
+    numberCount(NOTE_LIST, BOOK_LIST);
+  }, []);
 
   return (
     <S.Wrapper>
       <S.ButtonWrapper>
+        {/* clickedBtn이 true이면 note false이면 book */}
         <S.Button
           type="button"
           clickedBtn={clickedBtn}
-          onClick={() => handleClickBtn(clickedBtn)}
+          onClick={handleClickNoteBtn}
         >
-          레큐노트 (+999개)
+          레큐노트 ({noteCount}개)
         </S.Button>
         <S.Button
           type="button"
           clickedBtn={!clickedBtn}
-          onClick={() => handleClickBtn(clickedBtn)}
+          onClick={handleClickBookBtn}
         >
-          레큐북 (0개)
+          레큐북 ({bookCount}개)
         </S.Button>
       </S.ButtonWrapper>
 
-      <S.ListWrapper clickedBtn={clickedBtn}></S.ListWrapper>
+      <S.ListWrapper clickedBtn={clickedBtn}>
+        {clickedBtn
+          ? NOTE_LIST.map((note) => {
+              return (
+                <LecueNote
+                  key={note.noteId}
+                  favoriteName={note.favoriteName}
+                  title={note.title}
+                  noteDate={note.noteDate}
+                  content={note.content}
+                />
+              );
+            })
+          : BOOK_LIST.map((book) => {
+              return (
+                <LecueBook
+                  key={book.bookId}
+                  favoriteName={book.favoriteName}
+                  title={book.title}
+                  bookDate={book.bookDate}
+                  noteNum={book.noteNum}
+                />
+              );
+            })}
+      </S.ListWrapper>
     </S.Wrapper>
   );
 }
