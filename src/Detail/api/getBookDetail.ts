ff8631ffc5e6696a8ccb9ff9e682ab384bc92f92
eunbenn <ds5bean23@gmail.com>
2024-01-15 04:13:38 +0900

    chore: 테스트 uuid 변경

--- src/Detail/api/getBookDetail.ts
+++ src/Detail/api/getBookDetail.ts
@@ -2,7 +2,7 @@ import { api } from '../../libs/api';
 
 export async function getBookDetail() {
   const data = await api.get(
-    `/api/books/detail/ee4f66f9-9cf4-4b28-90f4-f71d0ecba024`,
+    `/api/books/detail/ee4f66f9-9cf4-4b28-90f4-f71d0ecba021`,
     {
       headers: {
         'Content-Type': 'application/json',
