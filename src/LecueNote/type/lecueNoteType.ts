64bf209b132e0aab5659a0c03c60a1dcab3cfc52
seoAreum <1971236@hansung.ac.kr>
2024-01-15 01:53:31 +0900

    chore: 타입 추가

--- src/LecueNote/type/lecueNoteType.ts
+++ src/LecueNote/type/lecueNoteType.ts
@@ -12,18 +12,21 @@ export interface SelectColorProps {
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => void;
   handleColorFn: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-  handleIconFn: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  handleIconFn: () => void;
+  uploadImage: (file: string) => void;
 }
 
 export interface ShowColorChartProps {
   isIconClicked: boolean;
   colorChart: string[];
   state: string;
+  uploadImage: (file: string) => void;
   handleFn: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-  handleIconFn: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  handleIconFn: () => void;
 }
 
 export interface WriteNoteProps extends CreateNoteProps {
+  imgFile: string;
   isIconClicked: boolean;
   clickedBgColor: string;
   clickedTextColor: string;
