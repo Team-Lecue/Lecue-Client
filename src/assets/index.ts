fce1573da28b47fd42f6a001c63b64923fe20646
jungwoo3490 <jungwoo3490@naver.com>
2024-01-14 09:43:36 +0900

    docs: svg 및 img 파일 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -10,6 +10,7 @@ import BtnKakaologin from './button/btn_kakaologin.svg?react';
 import IcArrowLeftBlack from './icon/ic_arrow_left_black.svg?react';
 import IcArrowLeftWhite from './icon/ic_arrow_left_white.svg?react';
 import IcCamera from './icon/ic_camera.svg?react';
+import IcCheck from './icon/ic_check.svg?react';
 import IcCrown from './icon/ic_crown.svg?react';
 import IcDate from './icon/ic_date.svg?react';
 import IcHome from './icon/ic_home.svg?react';
@@ -17,6 +18,9 @@ import IcNotice from './icon/ic_notice.svg?react';
 import IcSharing from './icon/ic_sharing.svg?react';
 import IcX from './icon/ic_x.svg?react';
 import ImgBook from './img/img_book.svg?react';
+import ImgBookBackgray from './img/img_book_backgray.svg?react';
+import ImgBookOrange from './img/img_book_orange.svg?react';
+import ImgBookOrangeBig from './img/img_book_orange_big.svg';
 import ImgKakao01 from './img/img_kakao_01.svg?react';
 import ImgKakao02 from './img/img_kakao_02.svg?react';
 import ImgKakao03 from './img/img_kakao_03.svg?react';
@@ -42,6 +46,7 @@ export {
   IcArrowLeftBlack,
   IcArrowLeftWhite,
   IcCamera,
+  IcCheck,
   IcCrown,
   IcDate,
   IcHome,
@@ -49,6 +54,9 @@ export {
   IcSharing,
   IcX,
   ImgBook,
+  ImgBookBackgray,
+  ImgBookOrange,
+  ImgBookOrangeBig,
   ImgKakao01,
   ImgKakao02,
   ImgKakao03,
