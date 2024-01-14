695d45972204f6dd4053cbb744cdbab85e454c76
Doyun Lee <doyn511@ajou.ac.kr>
2024-01-15 04:27:55 +0900

    feat: 레큐노트 클릭이벤트 지정

--- src/Mypage/components/LecueNote/index.tsx
+++ src/Mypage/components/LecueNote/index.tsx
@@ -3,6 +3,7 @@ import * as S from './LecueNote.style';
 
 function LecueNote(props: LecueNoteProps) {
   const {
+    noteId,
     favoriteName,
     title,
     noteDate,
@@ -12,10 +13,17 @@ function LecueNote(props: LecueNoteProps) {
     noteBackgroundImage,
   } = props;
 
+  const handleClickNote = (noteId: number) => {
+    alert(`${noteId}가 선택되었습니다.`);
+  };
+
   return (
     <S.Wrapper
       noteBackgroundColor={noteBackgroundColor}
       noteBackgroundImage={noteBackgroundImage}
+      onClick={() => {
+        handleClickNote(noteId);
+      }}
     >
       <S.TextWrapper noteTextColor={noteTextColor}>
         <S.Name>{favoriteName}</S.Name>
