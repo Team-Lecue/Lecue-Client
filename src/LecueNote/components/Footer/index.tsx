9ed4e985600f0c6c0d358db034621a11403a3693
seoAreum <1971236@hansung.ac.kr>
2024-01-15 13:49:56 +0900

    chore: 컴포넌트 내부 함수 분리

--- src/LecueNote/components/Footer/index.tsx
+++ src/LecueNote/components/Footer/index.tsx
@@ -1,35 +1,13 @@
-// import { useNavigate } from 'react-router-dom';
-
 import Button from '../../../components/common/Button';
-import { api } from '../../../libs/api';
+import postLecueNote from '../../api/postLecueNote';
 import { FooterProps } from '../../type/lecueNoteType';
 import * as S from './Footer.style';
 
 function Footer({ contents, fileName, textColor, bgColor }: FooterProps) {
-  // const navigate = useNavigate();
   const color = textColor === '#FFF' ? 1 : 0;
 
   const handleClickBtn = () => {
-    api
-      .post(
-        '/api/notes',
-        {
-          bookId: 1,
-          content: `${contents}`,
-          textColor: color,
-          background: fileName ? `${fileName}` : `${bgColor}`,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${import.meta.env.VITE_APP_TOKEN}`,
-          },
-        },
-      )
-      .then((res) => {
-        console.log(res);
-        // 나중에 주석코드를 활성화시킬 예정!
-        // navigate(`lecue-book/${res.data.data.bookUuid}`);
-      });
+    postLecueNote(contents, color, fileName, bgColor);
   };
 
   return (
--- src/LecueNote/components/ShowColorChart/index.tsx
+++ src/LecueNote/components/ShowColorChart/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useRef, useState } from 'react';
 
 import { IcCameraSmall } from '../../../assets';
-import { api } from '../../../libs/api';
+import getPresignedUrl from '../../api/getPresignedUrl';
+import handleImageUpload from '../../api/handleImageUpload';
 import { BG_COLOR_CHART } from '../../constants/colorChart';
 import { ShowColorChartProps } from '../../type/lecueNoteType';
 import * as S from './ShowColorChart.style';
@@ -18,45 +19,8 @@ function ShowColorChart({
   const imgRef = useRef<HTMLInputElement | null>(null);
   const [presignedUrl, setPresignedUrl] = useState('');
 
-  const getPresignedUrl = () => {
-    api.get('/api/images/note').then((res) => {
-      setPresignedUrl(res.data.data.url);
-      setFileName(res.data.data.fileName);
-    });
-  };
-
-  const handleImageUpload = () => {
-    const fileInput = imgRef.current;
-
-    if (fileInput && fileInput.files && fileInput.files.length > 0) {
-      const file = fileInput.files[0];
-
-      // reader1: 파일을 base64로 읽어서 업로드
-      const reader1 = new FileReader();
-      reader1.readAsDataURL(file);
-      reader1.onloadend = () => {
-        if (reader1.result !== null) {
-          uploadImage(reader1.result as string);
-        }
-      };
-
-      // reader2: 파일을 ArrayBuffer로 읽어서 PUT 요청 수행
-      const reader2 = new FileReader();
-      reader2.readAsArrayBuffer(file);
-      reader2.onloadend = () => {
-        if (reader2.result !== null && presignedUrl) {
-          api.put(presignedUrl, reader2.result, {
-            headers: {
-              'Content-Type': file.type,
-            },
-          });
-        }
-      };
-    }
-  };
-
   useEffect(() => {
-    getPresignedUrl();
+    getPresignedUrl(setPresignedUrl, setFileName);
   }, []);
 
   return (
@@ -66,7 +30,9 @@ function ShowColorChart({
           <S.Input
             type="file"
             accept="image/*"
-            onChange={handleImageUpload}
+            onChange={() =>
+              handleImageUpload(imgRef, uploadImage, presignedUrl)
+            }
             ref={imgRef}
           />
 
