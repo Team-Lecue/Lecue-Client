7a0b3d3a84162e9fb4ed903b56c073d72d67b6ad
eunbenn <ds5bean23@gmail.com>
2024-01-15 18:35:43 +0900

    fix: 센트리 주석

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,23 @@
-import * as Sentry from '@sentry/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 
-Sentry.init({
-  dsn: 'https://ff1721b5da5c3ed5ed8c4e4507b6ad51@o4506557086629888.ingest.sentry.io/4506573475741696',
-  integrations: [
-    new Sentry.BrowserTracing({
-      tracePropagationTargets: ['localhost', /^https:\/\/www\.lecue\.me/],
-    }),
-    new Sentry.Replay({
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
-  ],
-  tracesSampleRate: 1.0,
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
+// Sentry.init({
+//   dsn: 'https://ff1721b5da5c3ed5ed8c4e4507b6ad51@o4506557086629888.ingest.sentry.io/4506573475741696',
+//   integrations: [
+//     new Sentry.BrowserTracing({
+//       tracePropagationTargets: ['localhost', /^https:\/\/www\.lecue\.me/],
+//     }),
+//     new Sentry.Replay({
+//       maskAllText: false,
+//       blockAllMedia: false,
+//     }),
+//   ],
+//   tracesSampleRate: 1.0,
+//   replaysSessionSampleRate: 0.1,
+//   replaysOnErrorSampleRate: 1.0,
+// });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
