fe67c7094fb8b19f696c7c5bc243245da43f465a
Doyun Lee <doyn511@ajou.ac.kr>
2024-01-16 06:30:30 +0900

    feat: 임시 로딩 컴포넌트 생성

---
+++ src/Login/components/Loading.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { getLoginToken } from '../api/getLoginToken';
+import { postLoginToken } from '../api/postLoginToken';
+
+function Loading() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const tokenRes = await getLoginToken();
+
+        const { nickname, tokenDto } = await postLoginToken(tokenRes);
+
+        if (nickname === null) {
+          navigate('/register');
+        } else {
+          window.localStorage.setItem('token', tokenDto.accessToken);
+          window.localStorage.setItem('nickname', nickname);
+        }
+      } catch (error) {
+        console.error('로딩-fetchData() 에러 발생:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <div>로딩중.....</div>;
+}
+
+export default Loading;
