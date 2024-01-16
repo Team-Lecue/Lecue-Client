5a68540baab0e4137552be19579c3b4b590ea042
seoAreum <1971236@hansung.ac.kr>
2024-01-16 17:15:34 +0900

    feat: data가 존재하는 경우에만 map 실행하도록 수정

--- src/Home/components/LecueBookList/index.tsx
+++ src/Home/components/LecueBookList/index.tsx
@@ -24,17 +24,18 @@ function LecueBookList() {
     <S.LecueBookListWrapper>
       <S.Title>인기 레큐북 구경하기</S.Title>
       <S.LecueBookList>
-        {data.data.map((book: BookProps) => (
-          <S.LecueBook
-            key={book.bookId}
-            onClick={() =>
-              handleClickLecueBook(book.favoriteName, book.bookUuid)
-            }
-          >
-            <S.BookImage src={book.favoriteImage} alt="레큐북-이미지" />
-            <S.BookTitle>{book.favoriteName}</S.BookTitle>
-          </S.LecueBook>
-        ))}
+        {data &&
+          data.data.map((book: BookProps) => (
+            <S.LecueBook
+              key={book.bookId}
+              onClick={() =>
+                handleClickLecueBook(book.favoriteName, book.bookUuid)
+              }
+            >
+              <S.BookImage src={book.favoriteImage} alt="레큐북-이미지" />
+              <S.BookTitle>{book.favoriteName}</S.BookTitle>
+            </S.LecueBook>
+          ))}
       </S.LecueBookList>
     </S.LecueBookListWrapper>
   );
