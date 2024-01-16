67c1cc5cceb50cd521d507a0f5cbc71dc6f77904
Doyun Lee <doyn511@ajou.ac.kr>
2024-01-16 17:01:16 +0900

    feat: 닉네임 PATCH API 구현

---
+++ src/Register/api/patchNickname.ts
@@ -0,0 +1,17 @@
+import { api } from '../../libs/api';
+
+export const patchNickname = async (token: string, nickname: string) => {
+  const response = await api.patch(
+    '/api/nickname',
+    {
+      nickname: { nickname },
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+  return response;
+};
