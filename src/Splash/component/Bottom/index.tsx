0aa61265f8afecd1a463a8e294b794e7ce07f2b1
seoAreum <1971236@hansung.ac.kr>
2024-01-16 18:03:37 +0900

    feat: 로그인/ 회원가입 버튼 구현

--- src/Splash/component/Bottom/index.tsx
+++ src/Splash/component/Bottom/index.tsx
@@ -1,10 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
 import { StepProps } from '../../page/SplashPage';
 import * as S from './Bottom.style';
 
 function Bottom({ handleStep }: StepProps) {
+  const navigate = useNavigate();
+  const handleLoginBtn = () => {
+    navigate('/login');
+  };
+
   return (
-    <S.BottomWrapper type="button" onClick={() => handleStep(1)}>
-      시작하기
+    <S.BottomWrapper>
+      <S.StartBtn type="button" onClick={() => handleStep(1)}>
+        시작하기
+      </S.StartBtn>
+      <S.LoginBtn type="button" onClick={handleLoginBtn}>
+        로그인/회원가입
+      </S.LoginBtn>
     </S.BottomWrapper>
   );
 }
