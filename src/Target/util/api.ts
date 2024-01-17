b340f09133e5dcfcff66fec1a1ce25b513b7ce00
jungwoo3490 <jungwoo3490@naver.com>
2024-01-18 06:42:18 +0900

    feat: TargetPage React-Query 연동

--- src/Target/util/api.ts
+++ src/Target/util/api.ts
@@ -7,28 +7,42 @@ interface PresignedUrlResponse {
     url: string;
     fileName: string;
   };
+
+  // const getPresignedUrl = async () => {
+  //   const { data } = await api.get('/api/images/note');
+  //   return data;
+  // };
+
+  // export default getPresignedUrl;
 }
 
-const getPresignedUrl = async (
-  endpoint: string,
-): Promise<{ url: string; fileName: string }> => {
-  const response: AxiosResponse<PresignedUrlResponse> = await api.get(endpoint);
+const getPresignedUrl = async (): Promise<{
+  url: string;
+  fileName: string;
+}> => {
+  const response: AxiosResponse<PresignedUrlResponse> =
+    await api.get('/api/images/book');
+  console.log(response);
   return {
     url: response.data.data.url,
     fileName: response.data.data.fileName,
   };
 };
 
-const uploadFile = async (
+const putPresignedUrl = async (
   url: string,
   data: ArrayBuffer,
   contentType: string,
 ): Promise<void> => {
-  await api.put(url, data, {
-    headers: {
-      'Content-Type': contentType,
-    },
-  });
+  await api
+    .put(url, data, {
+      headers: {
+        'Content-Type': contentType,
+      },
+    })
+    .then((res) => {
+      console.log(res);
+    });
 };
 
-export { getPresignedUrl, uploadFile };
+export { getPresignedUrl, putPresignedUrl };
