32825ce6d69d0b1f3c207b8bd781c9758727e044
seoAreum <1971236@hansung.ac.kr>
2024-01-19 03:36:03 +0900

    feat: 모바일 뷰 리사이징 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import styled from '@emotion/styled';
+import { useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import Router from './Router';
@@ -17,6 +18,32 @@ const queryClient = new QueryClient({
 });
 
 function App() {
+  const setScreenSize = () => {
+    // vh 관련
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    // window width 관련
+    const windowWidth =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth;
+    const maxWidth = Math.min(375, windowWidth);
+    document.documentElement.style.setProperty(
+      '--app-max-width',
+      `${maxWidth}px`,
+    );
+  };
+
+  useEffect(() => {
+    setScreenSize();
+    window.addEventListener('resize', setScreenSize);
+
+    return () => {
+      window.removeEventListener('resize', setScreenSize);
+    };
+  }, []);
+
   return (
     <Wrapper>
       <QueryClientProvider client={queryClient}>
