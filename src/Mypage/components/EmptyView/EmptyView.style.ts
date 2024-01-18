4e75a4c4180e8286a26bcda77531cf99f158fbae
Doyun Lee <doyn511@ajou.ac.kr>
2024-01-18 19:51:54 +0900

    chore: stylelint 적용

--- src/Mypage/components/EmptyView/EmptyView.style.ts
+++ src/Mypage/components/EmptyView/EmptyView.style.ts
@@ -11,12 +11,12 @@ export const Wrapper = styled.div`
 
 export const TextWrapper = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
   flex-direction: column;
 
   width: 100%;
   height: 100%;
-  justify-content: center;
-  align-items: center;
 `;
 
 export const Text = styled.p`
