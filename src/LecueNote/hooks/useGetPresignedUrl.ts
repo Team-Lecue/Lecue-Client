3f51ec1bf3fbc67c30330f6fe6d07d2bfb0eda56
seoAreum <1971236@hansung.ac.kr>
2024-01-20 05:41:22 +0900

    fix: 레큐북 생성 후 노트 생성 시 에러 발생하는 이슈 해결

--- src/LecueNote/hooks/useGetPresignedUrl.ts
+++ src/LecueNote/hooks/useGetPresignedUrl.ts
@@ -3,13 +3,20 @@ import { useNavigate } from 'react-router-dom';
 
 import getPresignedUrl from '../api/getPresignedUrl';
 
-const useGetPresignedUrl = () => {
+const useGetPresignedUrl = (
+  setPresignedUrl: React.Dispatch<React.SetStateAction<string>>,
+  setFileName: React.Dispatch<React.SetStateAction<string>>,
+) => {
   const navigate = useNavigate();
 
   const { isLoading, data } = useQuery({
     queryKey: ['get-presigned-url'],
     queryFn: () => getPresignedUrl(),
     onError: () => navigate('/error'),
+    onSuccess: (data) => {
+      setPresignedUrl(data.data.url);
+      setFileName(data.data.fileName);
+    },
     refetchOnWindowFocus: false,
   });
 
