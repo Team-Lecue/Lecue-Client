e289dfa506b9b320dd47b3b7d4c976b03cce62cb
eunbeann <ds5bean23@gmail.com>
2024-02-07 12:14:50 +0900

    feat: pord 배포를 위한 스크립트 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     svgr(),
@@ -15,6 +16,7 @@ export default defineConfig({
   ],
 
   server: {
+    host: true,
     hmr: {
       port: 3000,
     },
