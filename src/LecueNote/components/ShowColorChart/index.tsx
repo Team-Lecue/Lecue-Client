e36dbaf01683097d381e92a8a8003fee5fd26ede
seoAreum <1971236@hansung.ac.kr>
2024-02-22 17:33:59 +0900

    feat: 파일 확장자 변환 로직 분리

--- src/LecueNote/components/ShowColorChart/index.tsx
+++ src/LecueNote/components/ShowColorChart/index.tsx
@@ -1,4 +1,3 @@
-import heic2any from 'heic2any';
 import { useRef } from 'react';
 
 import { IcCameraSmall } from '../../../assets';
@@ -7,6 +6,7 @@ import useGetPresignedUrl from '../../hooks/useGetPresignedUrl';
 import { ShowColorChartProps } from '../../type/lecueNoteType';
 import handleClickFiletoBinary from '../../util/handleClickFiletoBinary';
 import handleClickFiletoString from '../../util/handleClickFiletoString';
+import handleClickHeicToJpg from '../../util/handleClickHeicToJpg';
 import * as S from './ShowColorChart.style';
 
 function ShowColorChart({
@@ -18,6 +18,7 @@ function ShowColorChart({
   selectedFile,
   handleFn,
   handleIconFn,
+  handleIsLoading,
 }: ShowColorChartProps) {
   const imgRef = useRef<HTMLInputElement | null>(null);
   useGetPresignedUrl({ presignedUrlDispatch });
@@ -37,33 +38,12 @@ function ShowColorChart({
         file.name.split('.')[1] === 'heic' ||
         file.name.split('.')[1] === 'HEIC'
       ) {
-        // 'heic' 확장자인 경우에만 처리
-        heic2any({ blob: file, toType: 'image/jpeg' }).then(
-          function (resultBlob) {
-            // 변환된 Blob을 사용하여 새로운 File 생성
-            const jpg = new File(
-              Array.isArray(resultBlob) ? resultBlob : [resultBlob],
-              file.name.split('.')[0] + '.jpg',
-              { type: 'image/jpeg', lastModified: new Date().getTime() },
-            );
-
-            // reader1: 파일을 base64로 읽어서 업로드
-            const reader1 = new FileReader();
-            handleClickFiletoString({
-              file: jpg,
-              reader: reader1,
-              handleTransformImgFile,
-            });
-
-            // reader2: 파일을 ArrayBuffer로 읽어서 PUT 요청 수행
-            const reader2 = new FileReader();
-            handleClickFiletoBinary({
-              file: jpg,
-              reader: reader2,
-              handleReaderOnloadend,
-            });
-          },
-        );
+        handleClickHeicToJpg({
+          file: file,
+          handleTransformImgFile,
+          handleReaderOnloadend,
+          handleIsLoading,
+        });
       } else {
         const reader1 = new FileReader();
         handleClickFiletoString({
