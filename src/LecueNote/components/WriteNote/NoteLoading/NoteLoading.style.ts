44d0c72251d0f635967dd15f625fda8f9ba0cf51
seoAreum <1971236@hansung.ac.kr>
2024-02-22 19:41:39 +0900

    style: heic 형변환 시, 로딩 컴포넌트 -> 레큐노트 위에 위치시킴

--- src/LecueNote/components/WriteNote/NoteLoading/NoteLoading.style.ts
+++ src/LecueNote/components/WriteNote/NoteLoading/NoteLoading.style.ts
@@ -4,6 +4,9 @@ export const LoadingWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
 
   width: 100%;
   height: 100%;
