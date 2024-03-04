782850586f83fd152a479f21bbd6a58016cc617f
seoAreum <1971236@hansung.ac.kr>
2024-03-04 20:23:50 +0900

    feat: 로그인 여부에 따른 분기처리

--- src/Home/page/index.tsx
+++ src/Home/page/index.tsx
@@ -8,6 +8,7 @@ import useGetLecueBook from '../hooks/useGetLecueBook';
 import * as S from './Home.style';
 
 function Home({ handleStep }: StepProps) {
+  const token = localStorage.getItem('token');
   const { isLoading, data } = useGetLecueBook();
 
   useEffect(() => {
@@ -20,7 +21,7 @@ function Home({ handleStep }: StepProps) {
     <S.Wrapper>
       <NavigateLecueBook />
       {/* 서버 api 나오면 즐겨찾기 data props로 넘겨주는 부분 추가할 예정 */}
-      <LecueBookList title="즐겨찾기한 레큐북" />
+      {token && <LecueBookList title="즐겨찾기한 레큐북" />}
       <LecueBookList title="인기 레큐북 구경하기" data={data.data} />
     </S.Wrapper>
   );
