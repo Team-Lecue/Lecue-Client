d2b73599f6183e4c3a835e5f4013ee94c1fbe351
seoAreum <1971236@hansung.ac.kr>
2024-03-04 18:51:22 +0900

    feat: 즐겨찾기한 레큐북이 없는 경우 띄워줄 컴포넌트 생성

---
+++ src/Home/components/NoBookmarkList/index.tsx
@@ -0,0 +1,18 @@
+import * as S from './NoBookmarkList.style';
+
+const NoBookmarkList = () => {
+  return (
+    <S.ListWrapper>
+      <S.DescriptionWrapper>
+        <S.Description>아직 즐겨찾기한 레큐북이 없어요.</S.Description>
+        <S.Description>
+          자주 보고 싶은 레큐북을 즐겨찾기 해보세요.
+        </S.Description>
+      </S.DescriptionWrapper>
+
+      <S.NavigateBtn type="button">레큐북 보러가기</S.NavigateBtn>
+    </S.ListWrapper>
+  );
+};
+
+export default NoBookmarkList;
