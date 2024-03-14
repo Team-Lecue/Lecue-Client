e6c11114406659e3c19ea0b02817ed6f62c84aba
Doyun Lee <doyn511@ajou.ac.kr>
2024-03-14 18:01:48 +0900

    feat: 내기록보기 진입뷰 퍼블리싱

---
+++ src/Mypage/components/HistoryEnterView/index.tsx
@@ -0,0 +1,13 @@
+import * as S from './HistoryEnterView.style';
+
+function HistoryEnterView() {
+  return (
+    <S.Wrapper>
+      <S.Tab>즐겨찾기 한 레큐북</S.Tab>
+      <S.Tab>내가 만든 레큐북</S.Tab>
+      <S.Tab>내가 남긴 레큐노트</S.Tab>
+    </S.Wrapper>
+  );
+}
+
+export default HistoryEnterView;
