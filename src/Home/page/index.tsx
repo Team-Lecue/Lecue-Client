ded72362254fddae365baed9788cad1dd61cb3cc
seoAreum <1971236@hansung.ac.kr>
2024-03-15 13:55:04 +0900

    feat: 홈 화면에서 즐겨찾기 api 호출

--- src/Home/page/index.tsx
+++ src/Home/page/index.tsx
@@ -4,25 +4,29 @@ import LoadingPage from '../../components/common/LoadingPage';
 import { StepProps } from '../../Splash/page/SplashPage';
 import LecueBookList from '../components/LecueBookList';
 import NavigateLecueBook from '../components/NavigateLecueBook';
+import useGetFavorite from '../hooks/useGetFavorite';
 import useGetLecueBook from '../hooks/useGetLecueBook';
 import * as S from './Home.style';
 
 function Home({ handleStep }: StepProps) {
   const token = localStorage.getItem('token');
-  const { isLoading, data } = useGetLecueBook();
+  const { isLoading: isLoadingLecueBook, data: lecueBook } = useGetLecueBook();
+  const { isLoading: isLoadingFavorite, data: favorite } = useGetFavorite();
 
   useEffect(() => {
     handleStep(1);
   }, []);
 
-  return isLoading ? (
+  return isLoadingLecueBook || isLoadingFavorite ? (
     <LoadingPage />
   ) : (
     <S.Wrapper>
       <NavigateLecueBook />
-      {/* 서버 api 나오면 즐겨찾기 data props로 넘겨주는 부분 추가할 예정 */}
-      {token && <LecueBookList title="즐겨찾기한 레큐북" />}
-      <LecueBookList title="인기 레큐북 구경하기" data={data.data} />
+
+      {token && (
+        <LecueBookList title="즐겨찾기한 레큐북" data={favorite.data} />
+      )}
+      <LecueBookList title="인기 레큐북 구경하기" data={lecueBook.data} />
     </S.Wrapper>
   );
 }
