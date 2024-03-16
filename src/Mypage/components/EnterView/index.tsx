5c94d4132398b9d50c2b21285168db69438c11e6
Doyun Lee <doyn511@ajou.ac.kr>
2024-03-16 18:41:16 +0900

    feat: 뷰 수정사항 구현 및 로그인/비로그인 처리

--- src/Mypage/components/EnterView/index.tsx
+++ src/Mypage/components/EnterView/index.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import { IcArrowLeft } from '../../../assets';
 import useGetMyNickName from '../../hooks/useGetMyNickname';
 import * as S from './EnterView.style';
+
 function EnterView() {
+  const [nickname, setNickname] = useState('');
   const navigate = useNavigate();
-  const { myNickName } = useGetMyNickName();
-  const token = window.localStorage.getItem('token');
+  const { state } = useLocation();
+
+  if (state) {
+    const { myNickName } = useGetMyNickName();
+    if (nickname === '' || nickname !== myNickName) setNickname(myNickName);
+  }
 
   const handleClickNickname = () => {
     navigate('nickname');
@@ -23,21 +29,12 @@ function EnterView() {
 
   return (
     <React.Fragment>
-      {token ? (
-        <>
+      {state ? (
+        <React.Fragment>
           <S.NicknameWrapper>
-            <S.NicknameText>{myNickName}님, 안녕하세요</S.NicknameText>
+            <S.NicknameText>{nickname}님, 안녕하세요</S.NicknameText>
           </S.NicknameWrapper>
-        </>
-      ) : (
-        <S.NicknameWrapper onClick={handleClickLogin} variant={'login'}>
-          <S.NicknameText>로그인하세요</S.NicknameText>
-          <IcArrowLeft />
-        </S.NicknameWrapper>
-      )}
 
-      {token && (
-        <>
           <S.MenuWrapper>
             <S.SubTitle>프로필</S.SubTitle>
             <S.Tab onClick={handleClickNickname}>
@@ -50,31 +47,27 @@ function EnterView() {
             </S.Tab>
           </S.MenuWrapper>
           <S.Line />
-        </>
-      )}
 
-      <S.MenuWrapper>
-        <S.SubTitle>서비스 이용 방침</S.SubTitle>
-        <S.Link href="" rel="noreferrer">
-          <S.Text>팀 설명 보기</S.Text>
-          <IcArrowLeft />
-        </S.Link>
-        <S.Link
-          href="https://rileybyeon.notion.site/TEAM-LECUE-b7801fe345544442938d3e54980032e4?pvs=4"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <S.Text>공지사항</S.Text>
-          <IcArrowLeft />
-        </S.Link>
-        <S.Link href="" rel="noreferrer">
-          <S.Text>문의하기</S.Text>
-          <IcArrowLeft />
-        </S.Link>
-      </S.MenuWrapper>
+          <S.MenuWrapper>
+            <S.SubTitle>서비스 이용 방침</S.SubTitle>
+            <S.Link href="" rel="noreferrer">
+              <S.Text>팀 설명 보기</S.Text>
+              <IcArrowLeft />
+            </S.Link>
+            <S.Link
+              href="https://rileybyeon.notion.site/TEAM-LECUE-b7801fe345544442938d3e54980032e4?pvs=4"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <S.Text>공지사항</S.Text>
+              <IcArrowLeft />
+            </S.Link>
+            <S.Link href="" rel="noreferrer">
+              <S.Text>문의하기</S.Text>
+              <IcArrowLeft />
+            </S.Link>
+          </S.MenuWrapper>
 
-      {token && (
-        <>
           <S.Line />
           <S.MenuWrapper>
             <S.SubTitle>기타</S.SubTitle>
@@ -84,7 +77,34 @@ function EnterView() {
               <IcArrowLeft />
             </S.Tab>
           </S.MenuWrapper>
-        </>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <S.NicknameWrapper onClick={handleClickLogin} variant={'login'}>
+            <S.NicknameText>로그인하세요</S.NicknameText>
+            <IcArrowLeft />
+          </S.NicknameWrapper>
+
+          <S.MenuWrapper>
+            <S.SubTitle>서비스 이용 방침</S.SubTitle>
+            <S.Link href="" rel="noreferrer">
+              <S.Text>팀 설명 보기</S.Text>
+              <IcArrowLeft />
+            </S.Link>
+            <S.Link
+              href="https://rileybyeon.notion.site/TEAM-LECUE-b7801fe345544442938d3e54980032e4?pvs=4"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <S.Text>공지사항</S.Text>
+              <IcArrowLeft />
+            </S.Link>
+            <S.Link href="" rel="noreferrer">
+              <S.Text>문의하기</S.Text>
+              <IcArrowLeft />
+            </S.Link>
+          </S.MenuWrapper>
+        </React.Fragment>
       )}
     </React.Fragment>
   );
