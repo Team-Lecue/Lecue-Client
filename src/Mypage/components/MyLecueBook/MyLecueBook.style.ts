ae325270e99ecd211df3977a81ed774e7ebaa836
Doyun Lee <doyn511@ajou.ac.kr>
2024-03-18 17:49:54 +0900

    feat: MyLecueBook 컴포넌트 구현

---
+++ src/Mypage/components/MyLecueBook/MyLecueBook.style.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.section`
+  display: flex;
+  gap: 1.2rem;
+  flex-direction: column;
+
+  padding: 0 1.6rem 2rem;
+  margin-top: 7.9rem;
+`;
---
+++ src/Mypage/components/MyLecueBook/index.tsx
@@ -0,0 +1,33 @@
+import useGetMyBookList from '../../hooks/useGetMyBookList';
+import { LecueBookProps } from '../../types/myPageType';
+import LecueBook from '../LecueBook';
+import * as S from './MyLecueBook.style';
+
+function MyLecueBook() {
+  const { myBookList } = useGetMyBookList();
+
+  return (
+    <S.Wrapper>
+      {myBookList && myBookList.length !== 0 ? (
+        myBookList.map((book: LecueBookProps) => {
+          return (
+            <LecueBook
+              key={book.bookId}
+              bookUuid={book.bookUuid}
+              bookId={book.bookId}
+              favoriteName={book.favoriteName}
+              title={book.title}
+              bookDate={book.bookDate}
+              noteNum={book.noteNum}
+              // isFavorite = {book.isFavorite}
+            />
+          );
+        })
+      ) : (
+        <div>레큐북 0개....~</div>
+      )}
+    </S.Wrapper>
+  );
+}
+
+export default MyLecueBook;
