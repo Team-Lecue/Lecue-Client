d1a3cd237d335e82bde8bd4e2c30f41d2867b5ce
seoAreum <1971236@hansung.ac.kr>
2024-03-19 03:57:07 +0900

    refactor: refetch로 location.reload 대체

--- src/Home/hooks/useDeleteFavorite.ts
+++ src/Home/hooks/useDeleteFavorite.ts
@@ -1,16 +1,19 @@
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 
 import deleteFavorite from '../api/deleteFavorite';
 
 const useDeleteFavorite = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: (bookId: number) => {
       return deleteFavorite(bookId);
     },
     onError: () => navigate('/error'),
-    onSuccess: () => location.reload(),
+    onSuccess: () => {
+      queryClient.refetchQueries(['get-favorite'], { exact: true });
+    },
   });
   return mutation;
 };
