6b5a0782c11005df249a1fc5a9837a069dce08ab
Doyun Lee <doyn511@ajou.ac.kr>
2024-03-20 17:37:11 +0900

    chore: 컴포넌트명 변경(MyLecueBook, MyLecueBookList)

---
+++ src/Mypage/components/MyLecueBookList/index.tsx
@@ -0,0 +1,33 @@
+import useGetMyBookList from '../../hooks/useGetMyBookList';
+import { LecueBookProps } from '../../types/myPageType';
+import MyLecueBook from '../MyLecueBook';
+import * as S from './MyLecueBookList.style';
+
+function MyLecueBookList() {
+  const { myBookList } = useGetMyBookList();
+
+  return (
+    <S.Wrapper>
+      {myBookList && myBookList.length !== 0 ? (
+        myBookList.map((book: LecueBookProps) => {
+          return (
+            <MyLecueBook
+              key={book.bookId}
+              bookUuid={book.bookUuid}
+              bookId={book.bookId}
+              favoriteName={book.favoriteName}
+              title={book.title}
+              bookDate={book.bookDate}
+              noteNum={book.noteNum}
+              isFavorite={book.isFavorite}
+            />
+          );
+        })
+      ) : (
+        <div>레큐북 0개....~</div>
+      )}
+    </S.Wrapper>
+  );
+}
+
+export default MyLecueBookList;
