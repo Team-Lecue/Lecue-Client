53cf78e728274b449828674ed6e58502ae92f33d
jungwoo3490 <jungwoo3490@naver.com>
2024-03-26 09:13:33 +0900

    feat: 닉네임 수정 페이지 구현

--- src/EditNickname/components/NicknameInput/index.tsx
+++ src/EditNickname/components/NicknameInput/index.tsx
@@ -1,31 +1,55 @@
-import EditButton from '../EditButton';
+import { useEffect, useState } from 'react';
+
+import { NicknameInputProps } from '../../types/editNicknameTypes';
+import handleChangeInput from '../../utils/handleCheckInput';
 import * as S from './NicknameInput.style';
 
-interface NicknameInputProps {
-  nickname: string;
-  changeNickname: (nickname: string) => void;
-}
+function NicknameInput({
+  nickname,
+  isValid,
+  handleSetNickname,
+  handleSetIsActive,
+  handleSetIsValid,
+}: NicknameInputProps) {
+  const [wordCnt, setWordCnt] = useState(0);
+  const currentNickname: string = localStorage.getItem('nickname') || '';
 
-const handleClickEditButton = () => {};
-
-function NicknameInput({ nickname, changeNickname }: NicknameInputProps) {
-  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value.length <= 8) {
-      changeNickname(e.target.value);
-    }
+  const handleSetWordCnt = (wordCnt: number) => {
+    setWordCnt(wordCnt);
   };
+
+  useEffect(() => {
+    wordCnt >= 2 ? handleSetIsActive(true) : handleSetIsActive(false);
+  }, [wordCnt]);
+
   return (
     <S.NicknameInputWrapper>
-      <S.InputContainer isEmpty={nickname.length === 0}>
+      <S.InputContainer isEmpty={nickname.length === 0} isValid={isValid}>
         <S.Input
           type="text"
           placeholder="닉네임을 입력해주세요"
           value={nickname}
-          onChange={handleChangeInput}
+          onChange={(e) => {
+            handleChangeInput({
+              handleSetNickname,
+              handleSetWordCnt,
+              handleSetIsValid,
+              currentNickname,
+              e,
+            });
+          }}
         />
         <S.WordCount>({nickname.length}/8)</S.WordCount>
       </S.InputContainer>
-      <EditButton isActive={false} onClick={handleClickEditButton} />
+      {isValid === 'special' ? (
+        <S.WarnigMsg>특수문자/이모지는 사용 불가능해요</S.WarnigMsg>
+      ) : isValid === 'duplicate' ? (
+        <S.WarnigMsg>이미 있는 닉네임이에요</S.WarnigMsg>
+      ) : (
+        isValid === 'space' && (
+          <S.WarnigMsg>마지막 공백 제외 2자 이상 입력해주세요</S.WarnigMsg>
+        )
+      )}
     </S.NicknameInputWrapper>
   );
 }
--- src/EditNickname/page/EditNickname.style.ts
+++ src/EditNickname/page/EditNickname.style.ts
@@ -1,7 +1,12 @@
 import styled from '@emotion/styled';
 
 export const EditNicknameBodyWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
   width: 100%;
+  height: calc(100dvh - 5.4rem);
   padding: 0 1.6rem;
   margin-top: 5.4rem;
 `;
