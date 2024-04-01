bea21d394c877e4e01443bd61660a7bb880a5cd6
Doyun Lee <doyn511@ajou.ac.kr>
2024-04-01 21:34:53 +0900

    feat: 레큐북 공통 컴포넌트 구현

---
+++ src/components/common/LecueBook/index.tsx
@@ -0,0 +1,68 @@
+import { useNavigate } from 'react-router-dom';
+
+import { IcHomeFavoriteFilled } from '../../../assets';
+import useDeleteFavorite from '../../../libs/hooks/useDeleteFavorite';
+import * as S from './LecueBook.style';
+
+type bookType = 'favorite' | 'normal';
+type deleteType = 'mypage' | 'home';
+
+interface LecueBookProps {
+  bookId: number;
+  bookUuid: string;
+  favoriteImage: string;
+  favoriteName: string;
+  bookType: bookType;
+  deleteType?: deleteType;
+}
+
+function LecueBook(props: LecueBookProps) {
+  const {
+    bookId,
+    bookUuid,
+    favoriteImage,
+    favoriteName,
+    deleteType,
+    bookType,
+  } = props;
+
+  const navigate = useNavigate();
+
+  const MypageDeleteMutation = useDeleteFavorite('mypage');
+  const HomeDeleteMutation = useDeleteFavorite('home');
+
+  const handleClickFavoriteBtn = (
+    bookId: number,
+    deleteType: deleteType | undefined,
+  ) => {
+    deleteType === 'home'
+      ? HomeDeleteMutation.mutate(bookId)
+      : MypageDeleteMutation.mutate(bookId);
+  };
+
+  const handleClickBook = (bookUuid: string) => {
+    navigate(`/lecue-book/${bookUuid}`);
+  };
+
+  return (
+    <S.LecueBookWrapper>
+      <S.BookImage
+        src={favoriteImage}
+        alt="레큐북-이미지"
+        onClick={() => handleClickBook(bookUuid)}
+      />
+      {bookType === 'favorite' && (
+        <S.FavoriteButton
+          type="button"
+          onClick={() => handleClickFavoriteBtn(bookId, deleteType)}
+        >
+          <IcHomeFavoriteFilled />
+        </S.FavoriteButton>
+      )}
+
+      <S.Title>{favoriteName}</S.Title>
+    </S.LecueBookWrapper>
+  );
+}
+
+export default LecueBook;
