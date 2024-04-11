eb6e8b43df3992c35e3ba8fdbcf15e8252b14ea7
Doyun Lee <doyn511@ajou.ac.kr>
2024-04-11 16:58:45 +0900

    style: 일러스트 추가

--- src/HistoryEnter/page/index.tsx
+++ src/HistoryEnter/page/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import {
+  ImgMypageFavoriteLecueBook,
+  ImgMypageLetter,
+  ImgMypageMakeLecueBook,
+} from '../../assets';
 import Header from '../../components/common/Header';
 import * as S from './HistoryEnter.style';
 
@@ -8,9 +13,18 @@ function HistoryEnter() {
     <React.Fragment>
       <Header headerTitle="내 기록 보기" />
       <S.HistoryEnterPageBodyWrapper>
-        <S.Tab>즐겨찾기 한 레큐북</S.Tab>
-        <S.Tab>내가 만든 레큐북</S.Tab>
-        <S.Tab>내가 남긴 레큐노트</S.Tab>
+        <S.Tab variant="book">
+          <S.Text>즐겨찾기 한 레큐북</S.Text>
+          <ImgMypageFavoriteLecueBook />
+        </S.Tab>
+        <S.Tab variant="book">
+          <S.Text>내가 만든 레큐북</S.Text>
+          <ImgMypageMakeLecueBook />
+        </S.Tab>
+        <S.Tab variant="letter">
+          <S.Text>내가 남긴 레큐노트</S.Text>
+          <ImgMypageLetter />
+        </S.Tab>
       </S.HistoryEnterPageBodyWrapper>
     </React.Fragment>
   );
