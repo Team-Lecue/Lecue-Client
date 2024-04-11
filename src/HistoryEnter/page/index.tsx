eef57ec81da2aa68370dba673983163bcdf67f10
Doyun Lee <doyn511@ajou.ac.kr>
2024-04-11 17:04:04 +0900

    chore: 워딩 수정

--- src/HistoryEnter/page/index.tsx
+++ src/HistoryEnter/page/index.tsx
@@ -14,7 +14,7 @@ function HistoryEnter() {
       <Header headerTitle="내 기록 보기" />
       <S.HistoryEnterPageBodyWrapper>
         <S.Tab variant="book">
-          <S.Text>즐겨찾기 한 레큐북</S.Text>
+          <S.Text>즐겨찾기한 레큐북</S.Text>
           <ImgMypageFavoriteLecueBook />
         </S.Tab>
         <S.Tab variant="book">
@@ -22,7 +22,7 @@ function HistoryEnter() {
           <ImgMypageMakeLecueBook />
         </S.Tab>
         <S.Tab variant="letter">
-          <S.Text>내가 남긴 레큐노트</S.Text>
+          <S.Text>내가 남긴 레터</S.Text>
           <ImgMypageLetter />
         </S.Tab>
       </S.HistoryEnterPageBodyWrapper>
