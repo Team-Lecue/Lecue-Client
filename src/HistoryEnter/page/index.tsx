ddb1036c587b8810961af8f10f14105f6f42de6d
Doyun Lee <doyn511@ajou.ac.kr>
2024-04-13 19:37:26 +0900

    chore: 반복 요소 상수로 분리

--- src/HistoryEnter/page/index.tsx
+++ src/HistoryEnter/page/index.tsx
@@ -8,23 +8,33 @@ import {
 import Header from '../../components/common/Header';
 import * as S from './HistoryEnter.style';
 
+const HistoryEnterList = [
+  {
+    title: '즐겨찾기한 레큐북',
+    variant: 'book',
+    image: <ImgMypageFavoriteLecueBook />,
+  },
+  {
+    title: '내가 만든 레큐북',
+    variant: 'book',
+    image: <ImgMypageMakeLecueBook />,
+  },
+  { title: '내가 남긴 레터', variant: 'letter', image: <ImgMypageLetter /> },
+];
+
 function HistoryEnter() {
   return (
     <React.Fragment>
       <Header headerTitle="내 기록 보기" />
       <S.HistoryEnterPageBodyWrapper>
-        <S.Tab variant="book">
-          <S.Text>즐겨찾기한 레큐북</S.Text>
-          <ImgMypageFavoriteLecueBook />
-        </S.Tab>
-        <S.Tab variant="book">
-          <S.Text>내가 만든 레큐북</S.Text>
-          <ImgMypageMakeLecueBook />
-        </S.Tab>
-        <S.Tab variant="letter">
-          <S.Text>내가 남긴 레터</S.Text>
-          <ImgMypageLetter />
-        </S.Tab>
+        {HistoryEnterList.map((element) => {
+          return (
+            <S.Tab variant={element.variant} key={element.title}>
+              <S.Text>{element.title}</S.Text>
+              {element.image}
+            </S.Tab>
+          );
+        })}
       </S.HistoryEnterPageBodyWrapper>
     </React.Fragment>
   );
