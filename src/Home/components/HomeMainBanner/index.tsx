381fde695977084ee6f61b97e1722a12c971a7b4
seoAreum <1971236@hansung.ac.kr>
2024-04-18 18:01:57 +0900

    feat: 일러스트 관련 상수 -> 상수 파일로 분리

--- src/Home/components/HomeMainBanner/index.tsx
+++ src/Home/components/HomeMainBanner/index.tsx
@@ -1,14 +1,9 @@
 import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import {
-  IcProfile,
-  ImgHome01,
-  ImgHome02,
-  ImgHome03,
-  ImgLogoLecue,
-} from '../../../assets';
+import { IcProfile, ImgLogoLecue } from '../../../assets';
 import CommonModal from '../../../components/common/Modal/CommonModal';
+import { illustrationArr } from '../../constants/HomeIllustArr';
 import * as S from './HomeMainBanner.style';
 
 function NavigateLecueBook() {
@@ -18,31 +13,18 @@ function NavigateLecueBook() {
   const [curIdx, setCurIdx] = useState(0);
   const illustrationId = useRef(0);
 
-  const illustrationArr = [
-    {
-      img: <ImgHome01 key={'ImgHome01'} />,
-      key: 0,
-    },
-    {
-      img: <ImgHome02 key={'ImgHome02'} />,
-      key: 1,
-    },
-    {
-      img: <ImgHome03 key={'ImgHome03'} />,
-      key: 2,
-    },
-  ];
-
   const nextSlide = () => {
     setCurIdx(
-      curIdx === 2 ? illustrationId.current=0 : illustrationId.current += 1,
+      curIdx === 2
+        ? (illustrationId.current = 0)
+        : (illustrationId.current += 1),
     );
   };
 
   useEffect(() => {
     const interval = setInterval(() => {
       nextSlide();
-    }, 1000);
+    }, 1500);
 
     return () => clearInterval(interval);
   }, [illustrationId.current]);
---
+++ src/Home/constants/HomeIllustArr.tsx
@@ -0,0 +1,16 @@
+import { ImgHome01, ImgHome02, ImgHome03 } from '../../assets';
+
+export const illustrationArr = [
+  {
+    img: <ImgHome01 key={'ImgHome01'} />,
+    key: 0,
+  },
+  {
+    img: <ImgHome02 key={'ImgHome02'} />,
+    key: 1,
+  },
+  {
+    img: <ImgHome03 key={'ImgHome03'} />,
+    key: 2,
+  },
+];
