4529fa0adb9781128dad88e59b97ac4c1ff1d78a
seoAreum <1971236@hansung.ac.kr>
2024-04-20 05:22:42 +0900

    style: swiper 효과 변경 및 스타일 컴포넌트 지정

--- src/Home/components/HomeSwiper/index.tsx
+++ src/Home/components/HomeSwiper/index.tsx
@@ -1,8 +1,7 @@
 import 'swiper/css';
-import 'swiper/css/effect-fade';
 import 'swiper/css/pagination';
 
-import { Autoplay, EffectCreative, Pagination } from 'swiper/modules';
+import { Autoplay, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import { illustrationArr } from '../../constants/HomeIllustArr';
@@ -14,26 +13,16 @@ const HomeSwiper = () => {
       <Swiper
         slidesPerView={1}
         mousewheel={true}
-        autoplay={{ delay: 1700, disableOnInteraction: false }}
+        spaceBetween={10}
+        autoplay={{ delay: 2500, disableOnInteraction: false }}
         loop={true}
-        modules={[Autoplay, Pagination, EffectCreative]}
+        modules={[Autoplay, Pagination]}
         pagination={{ clickable: true }}
-        effect={'creative'}
-        creativeEffect={{
-          prev: {
-            translate: ['-20%', 0, -50],
-            opacity: 0.8,
-          },
-          next: {
-            translate: ['80%', 0, 0],
-            opacity: 0.8,
-          },
-        }}
       >
         {illustrationArr.map((illustration) => {
           return (
             <SwiperSlide key={illustration.key}>
-              <div className="slide">{illustration.img}</div>
+              <S.InnerSlider>{illustration.img}</S.InnerSlider>
             </SwiperSlide>
           );
         })}
