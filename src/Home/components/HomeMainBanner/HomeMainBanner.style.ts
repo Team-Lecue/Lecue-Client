99bdf0d7e15bf1c299450910c98d395eee45aeef
seoAreum <1971236@hansung.ac.kr>
2024-04-20 05:20:33 +0900

    chore: 불필요한 코드 삭제

--- src/Home/components/HomeMainBanner/HomeMainBanner.style.ts
+++ src/Home/components/HomeMainBanner/HomeMainBanner.style.ts
@@ -15,36 +15,9 @@ export const IconWrapper = styled.section`
   margin: 4rem 1.8rem 3.5rem 1.6rem;
 `;
 
-export const SwiperWrapper = styled.section`
-  display: flex;
-
-  .swiper {
-    display: flex;
-    scroll-behavior: smooth;
-    transition-timing-function: linear;
-  }
-
-  .slide {
-    padding-bottom: 2rem;
-  }
-
-  .swiper-pagination {
-    bottom: 0;
-  }
-
-  .swiper-pagination-bullet {
-    background-color: ${({ theme }) => theme.colors.LG};
-    opacity: 1;
-  }
-
-  .swiper-pagination-bullet-active {
-    background-color: ${({ theme }) => theme.colors.DG};
-  }
-`;
-
 export const Button = styled.button`
   padding: 2.1rem 9.4rem 2.2rem 8.8rem;
-  margin: 2rem 0 4rem;
+  margin: 2.7rem 0 4rem;
 
   border: 0.1rem solid ${({ theme }) => theme.colors.BG};
   border-radius: 0 0.2rem 0.2rem 0;
