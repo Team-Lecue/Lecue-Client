e3905dcee811889d8e007df1671c7bef6dea2ce0
seoAreum <1971236@hansung.ac.kr>
2024-04-20 16:42:02 +0900

    chore: 불필요한 코드 삭제

--- src/Home/constants/HomeIllustArr.tsx
+++ src/Home/constants/HomeIllustArr.tsx
@@ -2,15 +2,15 @@ import { ImgHome01, ImgHome02, ImgHome03 } from '../../assets';
 
 export const illustrationArr = [
   {
-    img: <ImgHome01 key={'ImgHome01'} />,
-    key: 0,
+    img: <ImgHome01 />,
+    key: 'ImgHome01',
   },
   {
-    img: <ImgHome02 key={'ImgHome02'} />,
-    key: 1,
+    img: <ImgHome02 />,
+    key: 'ImgHome02',
   },
   {
-    img: <ImgHome03 key={'ImgHome03'} />,
-    key: 2,
+    img: <ImgHome03 />,
+    key: 'ImgHome03',
   },
 ];
