f7e31126c24cc5e543d89f70e63aed2992cc3339
Doyun Lee <doyn511@ajou.ac.kr>
2024-04-26 00:12:50 +0900

    feat: reload > refetch 변경

--- src/History/hooks/useDeleteMyBook.ts
+++ src/History/hooks/useDeleteMyBook.ts
@@ -1,16 +1,21 @@
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 
 import { deleteMyBook } from '../api/deleteMyBook';
 
 const useDeleteMyBook = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: (noteId: number) => {
       return deleteMyBook(noteId);
     },
     onError: () => navigate('/error'),
-    onSuccess: () => location.reload(),
+    onSuccess: () => {
+      queryClient.refetchQueries(['get-my-lecueBook'], {
+        exact: true,
+      });
+    },
   });
   return mutation;
 };
