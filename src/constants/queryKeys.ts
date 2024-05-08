849bf5e146c50feb65b1107f63049819cb57b924
seoAreum <1971236@hansung.ac.kr>
2024-05-08 22:26:55 +0900

    feat: 즐겨찾기 관련 쿼리키 상수화

---
+++ src/constants/queryKeys.ts
@@ -0,0 +1,10 @@
+export const QUERY_KEY = {
+  favorite: {
+    getFavoriteHome: 'get-favorite',
+    getFavoriteMypage: 'get-mypage-favorite',
+    getFavoriteLecueBook: 'get-my-lecueBook',
+  },
+  nickname: {
+    getNickName: 'get-nickname',
+  },
+};
