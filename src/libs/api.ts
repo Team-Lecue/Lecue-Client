c59046551949987a16a2248165ec0557bd091db6
Doyun Lee <doyn511@ajou.ac.kr>
2024-06-02 00:31:00 +0900

    feat: 비로그인 상태일 때 axios default header 설정하지 않도록 개선

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,21 +1,22 @@
 import axios, { AxiosInstance } from 'axios';
 
-let apiInstance: AxiosInstance;
+let apiInstance: AxiosInstance | null;
 
 export const api = () => {
-  const token = sessionStorage.getItem('token');
-
   if (!apiInstance) {
     apiInstance = axios.create({
       baseURL: import.meta.env.VITE_APP_BASE_URL,
     });
   }
 
-  const headerToken =
-    apiInstance.defaults.headers.common.Authorization?.toString();
+  const token = sessionStorage.getItem('token');
+  if (token) {
+    const headerToken =
+      apiInstance.defaults.headers.common.Authorization?.toString();
 
-  if (!headerToken || token !== headerToken.split(' ')[1]) {
-    apiInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    if (!headerToken || token !== headerToken.split(' ')[1]) {
+      apiInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    }
   }
 
   return apiInstance;
