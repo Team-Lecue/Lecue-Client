e654e4d03cb4b52069397fec727913333360b136
Doyun Lee <doyn511@ajou.ac.kr>
2024-06-01 23:58:22 +0900

    feat: token 일치 여부 확인 로직 추가

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -10,7 +10,13 @@ export const api = () => {
       baseURL: import.meta.env.VITE_APP_BASE_URL,
     });
   }
-  apiInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+  const headerToken =
+    apiInstance.defaults.headers.common.Authorization?.toString();
+
+  if (!headerToken || token !== headerToken.split(' ')[1]) {
+    apiInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  }
 
   return apiInstance;
 };
